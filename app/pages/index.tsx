d234238d1ca4f8f050d5823ee3a7d6fa38dd0082
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-08 01:19:11 -0500

    Fixed array traversal in skills section

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -58,8 +58,8 @@ export default function Home() {
                     <Image src ={`/${programmingLanguage}.png`} width={25} height={25} alt="">{description}</Image>
                     </summary>
                   </details>
-                )
-            
+                )}
+              }
             </span>
           </div>
 
