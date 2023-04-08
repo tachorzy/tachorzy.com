b07cfec27e92f7eca482c110ddf3103bffd27dbb
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-08 00:19:21 -0500

    Added About Me

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter, Manrope } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import localFont from '@next/font/local'
 import NavBar from '../components/navbar'
@@ -17,6 +17,16 @@ export const ManropeExtraBold = localFont({
   weight: '500'
 })
 
+export const ManropeMedium = localFont({
+  src: '../public/fonts/Manrope/Manrope-Medium.otf',
+  weight: '500'
+})
+
+export const ManropeRegular = localFont({
+  src: '../public/fonts/Manrope/Manrope-Regular.otf',
+  weight: '500'
+})
+
 export default function Home() {
   return (
     <>
@@ -30,13 +40,27 @@ export default function Home() {
         <div className={styles.navBar}>
             <NavBar></NavBar>
         </div>
-        <div className={ManropeExtraBold.className}>
-          <h1 className="text-dusty lg:xl:text-landingPageTitle text-5xl font-bold lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-20 mt-12">Tariq<br/>Achor Zyad</h1>
-          <h2 className="text-sandstone lg:xl:text-2xl text-lg font-bold lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-4. mt-3 leading-relaxed">Hey, I'm a 3rd year Computer Science student at the University of Houston, <div className="text-dusty">Exploring the vast world of tech.</div></h2>
-          {/* <h3 className="text-dusty lg:xl:text-6xl text-2xl font-extrabold lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-6 mt-4">About Me</h3>
-          <p className="text-sandstone lg:xl:text-xl lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-4 mt-2 leading-relaxed"></p> */}
 
-        </div>
+        <section>
+          {/* HEADER SECTION */}
+          <div className={ManropeExtraBold.className}>
+            <h1 className="text-dusty lg:xl:text-landingPageTitle text-5xl font-bold lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-20 mt-12">Tariq<br/>Achor Zyad</h1>
+            <h2 className="text-sandstone lg:xl:text-2xl text-lg font-bold lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-4.5 mt-3 leading-relaxed w-4/5 ">Hey, I'm Tariq, a 3rd year Computer Science student at the University of Houston, <div className="text-dusty">Exploring the vast world of tech.</div></h2>
+          </div>
+
+          {/* ABOUT ME */}
+          <div className={ManropeMedium.className}>
+            <span className={ManropeExtraBold.className}>
+              <h3 className="text-dusty lg:xl:text-6xl text-2xl font-extrabold lg:xl:ml-24 ml-12 pt-2 lg:xl:mt-6 mt-4">About Me</h3>
+            </span>
+            <p className="text-sandstone lg:xl:text-xl font-medium lg:xl:ml-24 ml-12 pt-1.5 lg:xl:mt-2 mt-1.5 leading-loose w-3/5">I'm an aspiring web-developer with a tireless passion for front-end development. With that passion, I continously drive myself to garner new knowledge and experience in the space of computer science.</p>
+            <p className="text-sandstone lg:xl:text-xl font-medium lg:xl:ml-24 ml-12 pt-1.5 lg:xl:mt-2 mt-1.5 leading-loose w-3/5">Over the years, I've discovered my strengths in tailoring sleek and intuitive user interfaces and experiences in component-based development. And, since then I've used these strengths to create a confluence of my passions and interests. All of which you can explore here.</p>
+            <p className="text-sandstone lg:xl:text-xl font-medium lg:xl:ml-24 ml-12 pt-1.5 lg:xl:mt-2 mt-1.5 leading-loose w-3/5"></p>
+          </div>
+          <div className="">
+
+          </div>
+        </section>
       </main>
     </>
   )
