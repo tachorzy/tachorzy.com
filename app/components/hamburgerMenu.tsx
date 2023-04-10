dad53daffd599636d3de557d51811615f614adaf
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-10 12:28:36 -0500

    Hamburger menu now closes when you click outside of it

--- app/components/hamburgerMenu.tsx
+++ app/components/hamburgerMenu.tsx
@@ -1,17 +1,26 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import { useOnClickOutside } from 'usehooks-ts'
 import LightModeToggle from '../components/lightModeToggle'
 
 const HamburgerMenu = () => {
     const [isActive, setIsActive] = useState(false)
+    const menuRef = useRef<HTMLDivElement>(null)
+
+    const handleClickOutside = () => {
+        setIsActive(false)
+    }
+
+    useOnClickOutside(menuRef, handleClickOutside)
+
     return (
         <div>
             <button className="p-1.5 hover:bg-sandstone hover:bg-opacity-5 rounded-2xl cursor-pointer select-none" onClick = {() => setIsActive(!isActive)}>
                 <Image src="/hamburger-menu-Sandstone.svg" width={48} height={42} alt="" className="cursor-pointer"/>
             </button>
             { isActive ? (
-                <div className="w-56 h-58 self-end mr-12 bg-sandstone rounded-2xl fixed right-0 mt-2 shadow-xl">
+                <div className="w-56 h-58 self-end mr-12 bg-sandstone rounded-2xl fixed right-0 mt-2 shadow-xl" ref={menuRef}>
                     <div className="flex flex-col text-bark font-bold py-3">
                         <div className="flex flex-row gap-x-4 items-center hover:bg-dusty pl-6 py-2 select-none">      
                             <Image src="/aboutme.svg" width={24} height={24} alt="" className=""></Image>              
