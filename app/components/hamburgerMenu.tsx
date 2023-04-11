920d6ebf3b9aad9fff1daf019b558f2d4ac8357f
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-11 11:03:19 -0500

    Added smooth scrolling

--- app/components/hamburgerMenu.tsx
+++ app/components/hamburgerMenu.tsx
@@ -16,31 +16,41 @@ const HamburgerMenu = () => {
 
     return (
         <div>
-            <button className="p-1.5 hover:bg-sandstone hover:bg-opacity-5 rounded-2xl cursor-pointer select-none" onClick = {() => setIsActive(!isActive)}>
+            <button className="p-1.5 hover:bg-sandstone hover:bg-opacity-5 rounded-2xl cursor-pointer select-none border-transparent    " onClick = {() => setIsActive(!isActive)}>
                 <Image src="/hamburger-menu-Sandstone.svg" width={48} height={42} alt="" className="cursor-pointer"/>
             </button>
             { isActive ? (
                 <div className="w-56 h-58 self-end mr-12 bg-sandstone rounded-2xl fixed right-0 mt-2 shadow-xl" ref={menuRef}>
                     <div className="flex flex-col text-bark font-bold py-3">
                         <div className="flex flex-row gap-x-4 items-center hover:bg-dusty pl-6 py-2 select-none">      
-                            <Image src="/aboutme.svg" width={24} height={24} alt="" className=""></Image>              
-                            <Link href="/" className="w-full">About</Link>
+                            <Link href="#about" className="w-full flex flex-row gap-x-4 items-center">
+                                <Image src="/aboutme.svg" width={24} height={24} alt="" className=""></Image>              
+                                About
+                                </Link>
                         </div>
                         <div className="flex flex-row gap-x-4 items-center hover:bg-dusty pl-6 py-2 select-none">
-                            <Image src="/skills.svg" width={24} height={24} alt="" className=""></Image>              
-                            <Link href="/" className="w-full">Skills</Link>
+                            <Link href="#skills" className="w-full flex flex-row gap-x-4 items-center">
+                                <Image src="/skills.svg" width={24} height={24} alt="" className=""></Image>              
+                                Skills
+                            </Link>
                         </div>
                         <div className="flex flex-row gap-x-4 items-center hover:bg-dusty pl-6 py-2 select-none">
-                            <Image src="/projects.svg" width={24} height={24} alt="" className=""></Image>              
-                            <Link href="/" className="w-full">Projects</Link>
+                            <Link href="#project" className="w-full flex flex-row gap-x-4 items-center">
+                                <Image src="/projects.svg" width={24} height={24} alt="" className=""></Image>              
+                                Projects
+                            </Link>
                         </div>
                         <div className="flex flex-row gap-x-3 items-center hover:bg-dusty pl-6 py-2 select-none">
-                            <Image src="/dallah.svg" width={27} height={27} alt="" className=""></Image>              
-                            <Link href="/" className="w-full">Travel</Link>
+                            <Link href="#travel" className="w-full flex flex-row gap-x-4 items-center">
+                                <Image src="/dallah.svg" width={27} height={27} alt="" className=""></Image>              
+                                Travel    
+                            </Link>
                         </div>
                         <div className="flex flex-row gap-x-3 items-center hover:bg-dusty pl-6 py-2 select-none">
-                            <Image src="/contactme.svg" width={26} height={26} alt="" className=""></Image>              
-                            <Link href="/">Contact</Link>
+                            <Link href="#contact" className="w-full flex flex-row gap-x-4 items-center">
+                                <Image src="/contactme.svg" width={26} height={26} alt="" className=""></Image>              
+                                Contact
+                            </Link>
                         </div>
                         <div className="flex flex-row gap-x-[1.15rem] items-center pl-6 pt-3 pb-2.5 select-none">
                             {/* <Image src="/fanous.svg" width={36} height={36} alt="" className="pb-3 pr-[0.18rem]"></Image> */}
