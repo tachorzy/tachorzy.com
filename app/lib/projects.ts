a3ff7adc4b47b1a7e019c682d4e4eae0fddd8ab8
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-04-19 14:43:37 -0500

    Added github links to project gifs

--- app/lib/projects.ts
+++ app/lib/projects.ts
@@ -1,6 +1,6 @@
 export const projects: Map<string, {imageUrl: string, hostLink:string | null, gitHubLink: string}> = new Map([
-    ["Gibraltr", {imageUrl: "/passport_akira.gif", hostLink: "https://www.gibraltr.com/", gitHubLink: ""}],
-    ["StudyUp", {imageUrl: "/StudyAesthetic.gif", hostLink: null, gitHubLink: ""}],
-    ["Dunya", {imageUrl: "/plane-aesthetic.gif", hostLink: null, gitHubLink: ""}],
-    ["This Portfolio", {imageUrl: "/art-artist.gif", hostLink: null, gitHubLink: ""}]
+    ["Gibraltr", {imageUrl: "/passport_akira.gif", hostLink: "https://www.gibraltr.com/", gitHubLink: "https://github.com/tachorzy/Gibraltr"}],
+    ["StudyUp", {imageUrl: "/StudyAesthetic.gif", hostLink: null, gitHubLink: "https://github.com/tachorzy/StudyUp"}],
+    ["Dunya", {imageUrl: "/plane-aesthetic.gif", hostLink: null, gitHubLink: "https://github.com/tachorzy/Dunya-Flight-Searcher"}],
+    ["This Portfolio", {imageUrl: "/art-artist.gif", hostLink: null, gitHubLink: "https://github.com/tachorzy/tachorzy.com"}]
 ]);
