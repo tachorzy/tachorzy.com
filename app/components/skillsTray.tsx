66becc327bed35d400cf5a00c376888673bd2259
tachorzy <tachorzyad@gmail.com>
2023-05-09 00:17:51 -0500

    added responsiveness to skillsTray.tsx for medium sized screens

--- app/components/skillsTray.tsx
+++ app/components/skillsTray.tsx
@@ -7,13 +7,13 @@ const SkillsTray = () => {
 
     return(
         <div className={ManropeRegular.className}>
-              <div className="flex flex-row flex-wrap md:lg:xl:2xl:gap-x-8 gap-x-3 self-end">
+              <div className="flex flex-row flex-wrap md:mt-0.5 md:max-2xl:gap-x-8 gap-x-3 self-end">
                 {Array.from(skills.entries()).map(([programmingLanguage, description]) => {
                     return(
                         <div>
                             <div className="flex flex-col items-center">
-                                <Image src ={`/${programmingLanguage}.svg`} width="0" height="0" alt="" className="select-none md:lg:xl:2xl:w-9 md:lg:xl:2xl:h-9 w-6 h-6"></Image>
-                                <h1 className="text-dusty leading-relaxed pt-2 font-semibold md:lg:xl:2xl:text-base text-xxs">{programmingLanguage}</h1>
+                                <Image src ={`/${programmingLanguage}.svg`} width="0" height="0" alt="" className="select-none md:max-2xl:w-9 md:max-2xl:h-9 w-6 h-6"></Image>
+                                <h1 className="text-dusty leading-relaxed pt-2 font-semibold md:max-2xl:text-base text-xxs">{programmingLanguage}</h1>
                             </div>
                         </div>
                 )})}
