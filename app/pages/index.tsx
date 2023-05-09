f97cb0ec7d2b1d66c62706067091d3fb625a9e03
tachorzy <tachorzyad@gmail.com>
2023-05-09 00:56:16 -0500

    added responsiveness to Project pages for medium sized screens

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={ManropeRegular.className}>
-          <div className="fixed top-0 right-0 md:lg:xl:2xl:mt-12 md:lg:xl:2xl:mr-12 mt-6 mr-6 z-50">
+          <div className="fixed top-0 right-0 md:max-2xl:mt-12 md:max-2xl:mr-12 mt-6 mr-6 z-50">
             <HamburgerMenu/>
           </div>
         </div>
@@ -64,7 +64,7 @@ export default function Home() {
             <Image src="/T.png" width={60} height={60} className="absolute right-0 bottom-0 mr-12 mb-12 select-none z-0 md:hidden block" alt=""></Image>
 
           </div>
-          <div className="mt-[19rem] hidden md:block">
+          <div className="mt-[19rem] lg:mt-[23rem] hidden md:max-2xl:block">
             <NextPageButton nextPageReference={'#about'}></NextPageButton>
           </div>
         </section>
@@ -84,12 +84,12 @@ export default function Home() {
           </div>
           <div className={AmiriBoldSlanted.className}>
             <div className="grid grid-rows-1 justify-end h-full align-bottom">
-              <h4 className="lg:xl:2xl:text-9xl text-8xl text-dusty relative ml-auto bottom-0 md:xl:2xl:mr-12 mr-10 mt-5 align-bottom select-none z-0">١</h4>
+              <h4 className="lg:max-2xl:text-9xl text-8xl text-dusty relative ml-auto bottom-0 lg:max-2xl:mr-12 mr-10 mt-5 align-bottom select-none z-0">١</h4>
             </div>
           </div>
         </section>        
         
-        <div className="hidden md:block">
+        <div className="hidden md:max-2xl:block">
           <NextPageButton nextPageReference={'#projects'}></NextPageButton>
         </div>
   
@@ -108,28 +108,28 @@ export default function Home() {
               })}
           </div> */}
           <div className="flex flex-col gap-y-10 mt-2">
-            <div className="lg:xl:ml-24 ml-10 flex md:lg:xl:2xl:flex-row flex-col">
+            <div className="lg:xl:ml-24 ml-10 flex md:max-2xl:flex-row flex-col">
               <Link href="https://www.gibraltr.com/" target="_blank">
                 <Image 
                   src="/passport_akira.gif" 
                   height="0" 
                   width="0" 
                   alt="" 
-                  className="rounded-3xl sepia-75 contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:lg:xl:2xl:w-80 md:lg:xl:2xl:h-44 w-56 h-36"
+                  className="rounded-3xl sepia-75 contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:max-2xl:w-80 md:max-2xl:h-44 w-56 h-36"
                 >
                 </Image>
               </Link>
 
-              <div className="flex flex-col md:lg:xl:2xl:w-1/2 w-5/6">
+              <div className="flex flex-col md:max-2xl:w-1/2 w-5/6 md:max-2xl:ml-10">
                 <div className={ManropeMedium.className}>
-                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:lg:xl:2xl:pt-0 pt-3">Gibraltr</h5>
+                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:max-2xl:pt-0 pt-3">Gibraltr</h5>
                   <p className="text-mudbrick lg:xl:text-base text-xs  font-medium pt-2 pb-3 leading-relaxed">With ever changing travel restrictions worldwide, it's always best to know before you go. And that's why Gibraltr provides a robust search engine for travelers to find visa requirements. Available for 197+ countries! With over 30,000 possible results!</p>
                   <ProjectTechTray techStack={["Next.js", "Javascript", "TailwindCSS"]} gitHubLink={"https://github.com/tachorzy/Gibraltr"} hostingLink={"https://www.gibraltr.com/"}></ProjectTechTray>
                 </div>
               </div>
             </div>
 
-            <div className="lg:xl:ml-24 ml-10 flex md:lg:xl:2xl:flex-row flex-col">
+            <div className="lg:xl:ml-24 ml-10 flex md:max-2xl:flex-row flex-col">
               
               <Link href="https://github.com/tachorzy/COSC4353-Project-Group39" target="_blank">
                 <Image 
@@ -137,13 +137,13 @@ export default function Home() {
                   width="0"
                   height="0"
                   alt=""
-                  className="rounded-3xl sepia-75 contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:lg:xl:2xl:w-80 md:lg:xl:2xl:h-44 w-56 h-36"
+                  className="rounded-3xl sepia-75 contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:max-2xl:w-80 md:max-2xl:h-44 w-56 h-36"
                 ></Image>
               </Link>
 
-              <div className="flex flex-col md:lg:xl:2xl:w-1/2 w-5/6">
+              <div className="flex flex-col md:max-2xl:w-1/2 w-5/6 md:max-2xl:ml-10">
                 <div className={ManropeMedium.className}>
-                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:lg:xl:2xl:pt-0 pt-3">This Portfolio</h5>
+                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:max-2xl:pt-0 pt-3">This Portfolio</h5>
                   <p className="text-mudbrick lg:xl:text-base text-xs  font-medium pt-2 pb-3 leading-relaxed">I've designed this page as a space to present my professional work in one accessible place. And in this one place, my passions for web-development and design meet. Here you'll find my projects, blogs, and where to best reach me.</p>
                   <ProjectTechTray techStack={["Next.js", "Javascript", "TailwindCSS"]} gitHubLink={"https://github.com/tachorzy/tachorzy.com"} hostingLink={"https://tachorzy.com/"}></ProjectTechTray>
                 </div>
@@ -152,33 +152,33 @@ export default function Home() {
             
             <div className={AmiriBoldSlanted.className}>
               <div className="grid grid-rows-1 justify-end h-full align-bottom">
-                  <h4 className="lg:xl:2xl:text-9xl text-8xl text-dusty relative ml-auto bottom-0 mr-12 align-bottom select-none z-0">٢</  h4>
+                  <h4 className="lg:max-2xl:text-9xl text-8xl text-dusty relative ml-auto bottom-0 mr-12 align-bottom select-none z-0">٢</  h4>
               </div>
             </div>
           </div>
         </section>
         
-        <div className="mt-32 hidden md:block">
-            <NextPageButton nextPageReference={'#more-projects'}></NextPageButton>
+        <div className="hidden md:max-2xl:block">
+          <NextPageButton nextPageReference={'#more-projects'}></NextPageButton>
         </div>
 
         <section id="more-projects" className = "relative h-screen">
           <div className="flex flex-col gap-y-10 lg:xl:mt-32">
-            <div className="lg:xl:ml-24 ml-10 flex md:lg:xl:2xl:flex-row flex-col">
+            <div className="lg:xl:ml-24 ml-10 flex md:max-2xl:flex-row flex-col">
               <Link href="https://github.com/tachorzy/CodeRED-Redacted" target="_blank">
                 <Image 
                   src="/aestheticGear.gif" 
                   height="0" 
                   width="0"
                   alt="" 
-                  className="rounded-3xl sepia-[.9] contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:lg:xl:2xl:w-80 md:lg:xl:2xl:h-44 w-56 h-36"
+                  className="rounded-3xl sepia-[.9] contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:max-2xl:w-80 md:max-2xl:h-44 w-56 h-36"
                 >
                 </Image>
               </Link>
 
-              <div className="flex flex-col md:lg:xl:2xl:w-1/2 w-5/6">
+              <div className="flex flex-col md:max-2xl:w-1/2 w-5/6 md:max-2xl:ml-10">
                 <div className={ManropeMedium.className}>
-                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:lg:xl:2xl:pt-0 pt-3">Pipeline</h5>
+                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:max-2xl:pt-0 pt-3">Pipeline</h5>
                     <p className="text-mudbrick lg:xl:text-base text-xs font-medium pt-2 pb-3 leading-relaxed">A web app made to enable companies in calculating fuel quotes, keeping track of orders, and managing fuel purchases. Pipeline provides an approachable user-friendly design for any business to navigate, whether small or large.</p>
                     <ProjectTechTray techStack={["Next.js", "JavaScript", "TailwindCSS"]} gitHubLink={"https://github.com/tachorzy/COSC4353-Project-Group39"} hostingLink={""}></ProjectTechTray>
                 </div>
@@ -186,20 +186,20 @@ export default function Home() {
             </div>
             
             <div className="flex flex-col gap-y-10 mt-2">
-              <div className="md:max-2xl:ml-24 ml-10 flex md:lg:xl:2xl:flex-row flex-col">
+              <div className="md:max-2xl:ml-24 ml-10 flex md:max-2xl:flex-row flex-col">
                 <Link href="https://github.com/tachorzy/battuta.ai" target="_blank">
                   <Image 
                     src="/plane-aesthetic.gif" 
                     height="0" 
                     width="0"
                     alt="" 
-                    className="rounded-3xl sepia-[.75] contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:lg:xl:2xl:w-80 md:lg:xl:2xl:h-44 w-56 h-36"
+                    className="rounded-3xl sepia-[.75] contrast-125 hover:sepia-0 hover:contrast-100 duration-1000 bg-blend-luminosity md:max-2xl:w-80 md:max-2xl:h-44 w-56 h-36"
                   >
                   </Image>
                </Link>
-                <div className="flex flex-col md:lg:xl:2xl:w-1/2 w-5/6">
+                <div className="flex flex-col md:max-2xl:w-1/2 w-5/6 md:max-2xl:ml-10">
                   <div className={ManropeMedium.className}>
-                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:lg:xl:2xl:pt-0 pt-3">Battuta.ai</h5>
+                  <h5 className="text-mudbrick lg:xl:text-4xl text-2xl font-extrabold md:max-2xl:pt-0 pt-3">Battuta.ai</h5>
                   <p className="text-mudbrick lg:xl:text-base text-xs font-medium pt-2 pb-3 leading-relaxed">Often times, itinerary planning can leave you lost in a myriad of options. Battuta is a website powered by OpenAI to provide you streamlined itineraries. Pick a starting city, list anywhere else you'll like to visit and Battuta will create a painless itinerary for your dream trip.</p>
                   <ProjectTechTray techStack={["Next.js", "TypeScript", "TailwindCSS"]} gitHubLink={"https://github.com/tachorzy/battuta.ai"} hostingLink={""}></ProjectTechTray>
                   </div>
@@ -209,12 +209,12 @@ export default function Home() {
           </div>
           <div className={AmiriBoldSlanted.className}>
             <div className="grid grid-rows-1 justify-end h-full align-bottom">
-                <h4 className="lg:xl:2xl:text-9xl text-8xl text-dusty relative ml-auto bottom-0 mr-12 mt-20 align-bottom select-none z-0">٣</  h4>
+                <h4 className="lg:max-2xl:text-9xl text-8xl text-dusty relative ml-auto bottom-0 mr-12 mt-20 align-bottom select-none z-0">٣</  h4>
             </div>
           </div>
         </section>
 
-        <div className="hidden md:block">
+        <div className="hidden md:max-2xl:block">
           <NextPageButton nextPageReference={'#travel'}></NextPageButton>      
         </div>
 
