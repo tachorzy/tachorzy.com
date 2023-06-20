98e84d628ee7f5b33440bbfa231e4a23735911f8
tachorzy <tachorzyad@gmail.com>
2023-06-20 16:38:01 -0500

    Adjusted files

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         <section className = "relative h-screen">
           {/* HEADER SECTION */}
           <div className={ManropeExtraBold.className}>
-            <h1 className=" text-dusty lg:xl:2xl:text-landingPageTitle md:text-7xl text-5xl font-bold md:lg:xl:ml-24 ml-10 md:lg:xl:2xl:mt-28 mt-32">Tariq<br/>Achor Zyad</h1>
+            <h1 className=" text-dusty md:max-2xl:text-landingPageTitle text-5xl font-bold md:lg:xl:ml-24 ml-10 md:lg:xl:2xl:mt-28 mt-32">Tariq<br/>Achor Zyad</h1>
             <h2 className="text-sandstone lg:xl:text-2xl md:text-2xl text-landingPageBioMobile font-bold md:lg:xl:ml-24 ml-10 pt-2 lg:xl:mt-5 mt-4 leading-relaxed w-4/5 ">Hey, I'm Tariq, a 3rd year Computer Science student at the University of Houston, <div className="text-dusty">Exploring the vast world of tech.</div></h2>
 
             {/* SOCIALS TRAY */}
@@ -64,7 +64,7 @@ export default function Home() {
             <Image src="/T.png" width={60} height={60} className="absolute right-0 bottom-0 mr-12 mb-12 select-none z-0 md:hidden block" alt=""></Image>
 
           </div>
-          <div className="mt-[19rem] lg:mt-[23rem] hidden md:max-2xl:block">
+          <div className="mt-[19rem] lg:mt-[21.5rem] hidden md:max-2xl:block">
             <NextPageButton nextPageReference={'#about'}></NextPageButton>
           </div>
         </section>
