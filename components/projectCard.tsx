d29f61151cf281664795694e71f8a9adefca5218
tachorzy <tachorzyad@gmail.com>
2023-06-26 00:06:32 -0500

    refactored ProjectCard component

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -15,19 +15,17 @@ interface project{
 const ProjectCard = (props: project) => {
     const tempHostLink: string | null = props.hostLink
     return(
-        <div className={ManropeExtraBold.className}>
-            <div className="mt-0.5 w-[17rem] h-80 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500">
-                <Link href={ tempHostLink != null ? tempHostLink : props.gitHubLink } target="_blank" className="w-full h-2/3">
-                    <Image src={props.imageUrl} height="0" width={350} alt="aesthetic gif" className="rounded-t-md h-[56%]"></Image>
-                </Link>
-                <div className="justify-center content-center pt-4 px-6 pb-1">
-                    <p className="text-dusty text-2xl">{props.name}</p>
-                    <div className={ManropeExtraBold.className}>
-                        <p className="text-mudbrick text-lg">{props.description}</p>
-                    </div>
-                    <div className="my-2 mb-3">
-                        <ProjectTechTray techStack={props.techStack} gitHubLink={props.gitHubLink} hostLink={props.hostLink}/>
-                    </div>
+        <div className={ManropeExtraBold.className + " mt-0.5 w-[17rem] h-80 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500"}>
+            <Link href={ tempHostLink != null ? tempHostLink : props.gitHubLink } target="_blank" className="w-full h-2/3">
+                <Image src={props.imageUrl} height="0" width={350} alt="travel gif" className="rounded-t-md h-[56%]"></Image>
+            </Link>
+            <div className="justify-center content-center pt-4 px-6 pb-1">
+                <p className="text-dusty text-2xl">{props.name}</p>
+                <div className={ManropeExtraBold.className}>
+                    <p className="text-mudbrick text-lg">{props.description}</p>
+                </div>
+                <div className="my-2 mb-3">
+                    <ProjectTechTray techStack={props.techStack} gitHubLink={props.gitHubLink} hostLink={props.hostLink}/>
                 </div>
             </div>
         </div>
