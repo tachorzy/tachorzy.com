931058f004aac1122ed4aeb37a98a159240c9e53
tachorzy <tachorzyad@gmail.com>
2023-07-01 22:28:45 -0500

    Added AboutMe component

--- components/about/AboutMe.tsx
+++ components/about/AboutMe.tsx
@@ -1,6 +1,6 @@
 import { ManropeMedium } from '../../lib/localNextFonts'
 
-const LandingPageHeaders = () => {
+const AboutMe = () => {
     return(
         <div className={ManropeMedium.className + " text-sandstone lg:max-2xl:text-xl md:text-lg text-xs font-medium pt-1.5 flex flex-col gap-y-4 lg:max-xl:mt-2 mt-1.5 leading-loose md:max-2xl:w-3/5 w-4/5"}>
             <p>I'm a developer with a tireless passion for full-stack web development. With that passion, I continously drive myself to learn, grow and create.</p>
@@ -10,4 +10,4 @@ const LandingPageHeaders = () => {
     );
 }
 
-export default LandingPageHeaders;
+export default AboutMe;
