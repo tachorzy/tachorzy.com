fc4c6f10f2ddefb57bf407ed24a71cc012c3b964
tachorzy <tachorzyad@gmail.com>
2023-07-05 02:43:44 -0500

    Added Questv1 font for success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,15 +1,16 @@
 import React, { ReactElement } from 'react'
 import styles from '../../styles/Home.module.css'
-import { ManropeExtraBold, ManropeRegular, ManropeMedium, AmiriBoldSlanted } from '../../lib/localNextFonts'
+import { ManropeExtraBold, ManropeRegular, ManropeMedium, AmiriBoldSlanted, RubikMedium, Quest } from '../../lib/localNextFonts'
 
 export default function Home() {
 
   return (
     <> 
       <main className={styles.main}>
-            <div className={ManropeMedium.className + ` h-screen text-center flex flex-col justify-center content-center`}>
+            <div className={ManropeMedium.className + ` h-screen text-center flex flex-col gap-y-2 justify-center content-center self-center w-1/2`}>
                 <h1 className="text-sandstone lg:xl:text-7xl md:text-6xl text-5xl font-semibold leading-relaxed ">ⵜⴰⵏⵎⵎⵉⵔⵜ</h1>
-                <h1 className={AmiriBoldSlanted.className + ` text-sandstone lg:xl:text-8xl md:text-7xl text-5xl leading-relaxed`}>شكرا بزاف</h1>
+                <h1 className={Quest.className + ` text-sandstone lg:xl:text-8xl md:text-7xl text-5xl leading-relaxed`}>شكرا بزاف</h1>
+                <h2 className="text-sandstone lg:xl:text-4xl md:text-3xl text-2xl leading-relaxed mx-auto">Your message has been submitted!<br/>I'll make sure to get back to you super soon.</h2>
             </div>
       </main>
     </>
