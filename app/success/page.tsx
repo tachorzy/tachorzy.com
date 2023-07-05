cb5e4ff8a5c95fa75991c7966f79860f0d750ddd
tachorzy <tachorzyad@gmail.com>
2023-07-05 16:48:12 -0500

    infinite header glitch

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -7,23 +7,26 @@ import { ManropeMedium, AmiriBoldSlanted, Quest } from '../../lib/localNextFonts
 
 
 export default function Home() {
-    const thankYouVariants: string[] = ["شكرا بزاف", "ⵜⴰⵏⵎⵎⵉⵔⵜ", "Merci beaucoup", "Thank You" ]
     const [thankYouMessage, setThankYouMessage] = useState("شكرا بزاف")
-    
+    const [animationKey, setAnimationKey] = useState(0);
+
     useEffect(() => {
         const interval = setInterval(() =>{
-            let index: number = 0
-            setThankYouMessage(thankYouVariants[index + 1 % thankYouVariants.length])
-        }, 5000)
+            setThankYouMessage('Thank You')
+            setAnimationKey((prevKey) => prevKey + 1);
+        }, 3000)
+        return () => clearInterval(interval);
     }, [])
     
-    return (
+
+    return (        
         <> 
             <main className={styles.main}>
                 <div className={ManropeMedium.className + ` h-screen text-center flex flex-col gap-y-2 justify-center content-center self-center w-1/2`}>
                     <div className="relative w-3/4 self-center">
-                        <h1 className={Quest.className + ` text-sandstone lg:xl:text-8xl md:text-7xl text-5xl py-4 leading-relaxed`}>{thankYouMessage}</h1>
+                        <h1 className={Quest.className + ` text-sandstone lg:xl:text-8xl md:text-7xl text-5xl py-4 leading-relaxed select-none`} key={animationKey}>{thankYouMessage}</h1>
                         <motion.div
+                            key={animationKey}
                             variants={{
                                 hidden: { right: 0 },
                                 visible: { right: "100%" },
@@ -32,7 +35,7 @@ export default function Home() {
                             initial="hidden"
                             animate="visible"
                             exit={{ opacity: 0 }}
-                            transition={{time: 6, duration: 0.5, ease: "easeIn", repeat: Infinity}}
+                            transition={{time: 6, duration: 0.5, ease: "easeIn"}}
                             style={{
                                 position: "absolute",
                                 top: 4, 
@@ -40,19 +43,18 @@ export default function Home() {
                                 left: 0,
                                 right: 0,
                                 background: "#1c2324",
-                                zIndex: 50,
+                                zIndex: 40,
                             }}
                         />
-                        <motion.div
+                        {/* <motion.div
                             variants={{
                                 hidden: { left: "100%" },
-                                visible: { left: 0 },
-                                
+                                visible: { left: 0 },         
                             }}
                             initial="hidden"
                             animate="visible"
                             exit={{ opacity: 0 }}
-                            transition={{time: 6, duration: 0.5, delay: 3, ease: "easeIn", repeat: Infinity}}
+                            transition={{duration: 0.5, delay: 3, ease: "easeIn"}}
                             style={{
                                 position: "absolute",
                                 top: 4, 
@@ -60,9 +62,9 @@ export default function Home() {
                                 left: 0,
                                 right: 0,
                                 background: "#1c2324",
-                                zIndex: 50,
+                                zIndex: 40,
                             }}
-                        />
+                        /> */}
                     </div>
                     <h2 className="text-sandstone lg:xl:text-4xl md:text-3xl text-2xl leading-relaxed mx-auto">Your message has been submitted!<br/>I'll make sure to get back to you super soon.</h2> 
                 </div>
