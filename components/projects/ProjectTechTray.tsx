c7023efe8c3292f286349ffc601d0e40d21af98b
tachorzy <tachorzyad@gmail.com>
2023-07-11 23:00:51 -0500

    Refactored breakpoints in Projects components  (removed max-2xl: prefix, and redudant prefixes)

--- components/projects/ProjectTechTray.tsx
+++ components/projects/ProjectTechTray.tsx
@@ -12,18 +12,18 @@ const ProjectTechTray = (props: projectTechStack) => {
     console.log(props.hostLink)
 
     return(
-        <div className="flex flex-row md:max-2xl:gap-x-3.5 gap-x-1 my-5 mb-3">
+        <div className="flex flex-row md:gap-x-3.5 gap-x-1 my-5 mb-3">
             {props.techStack.map((tool, index) => (
-                <Image src={`/icons/${tool}.svg`} width="0" height="0" alt="language/library/framework icon" className="md:max-2xl:w-6 md:max-2xl:h-6 w-6 h-6"/>
+                <Image src={`/icons/${tool}.svg`} width="0" height="0" alt="language/library/framework icon" className="md:w-6 md:h-6 w-6 h-6"/>
             ))}
             
-            <div className="relative md:max-2xl:gap-x-3.5">
-                <Link href={props.gitHubLink} className="group absolute align-middle justify-self-end text-dusty hover:text-mudbrick font-semibold md:max-2xl:text-sm text-sm pt-2 flex-none px-1.5" target="_blank">
+            <div className="relative md:gap-x-3.5">
+                <Link href={props.gitHubLink} className="group absolute align-middle justify-self-end text-dusty hover:text-mudbrick font-semibold md:text-sm text-sm pt-2 flex-none px-1.5" target="_blank">
                     GitHub
                     <span className="pl-0.5 group-hover:pl-1.5 duration-500">›</span>
                 </Link>
                 {props.hostLink &&
-                    <Link href={props.hostLink} className="group absolute left-20 align-middle justify-self-end text-dusty hover:text-mudbrick font-semibold md:max-2xl:text-sm text-sm pt-2 flex-none" target="_blank">
+                    <Link href={props.hostLink} className="group absolute left-20 align-middle justify-self-end text-dusty hover:text-mudbrick font-semibold md:text-sm text-sm pt-2 flex-none" target="_blank">
                         Visit
                         <span className="pl-0.5 group-hover:pl-1.5 duration-500">›</span>
                     </Link>
--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="flex flex-col md:max-2xl:flex-row gap-y-12 md:max-2xl:gap-x-12 flex-wrap"> 
+        <div className="flex flex-col md:flex-row gap-y-12 md:gap-x-12 flex-wrap"> 
             {Array.from(PROJECTS.entries()).map(([projectName, projectDetails]) => {
                 return(
                     <ProjectCard name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
