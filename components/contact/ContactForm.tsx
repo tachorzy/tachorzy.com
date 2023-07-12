8d8a819388ca226772de94de2187578792576b86
tachorzy <tachorzyad@gmail.com>
2023-07-12 02:10:59 -0500

    Refactored breakpoints on Contact components (removed max-2xl: prefix, and redudant prefixes)

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -44,7 +44,7 @@ const ContactForm = () => {
                         <label key={key} className="flex flex-col my-2 duration-500 focus-within:p-1 focus-within:pl-3 focus-within:border-l-4 border-sandstone border-inherit rounded">
                             <span className="text-dune md:text-xl text-lg pb-1.5">{labelText}</span>
                                 {labelText == "Your message:" 
-                                    ? <textarea name="message" value={contactInfo.message} onChange={handleChange} className=" pl-1.5 py-0.5 group bg-shark h-auto duration-700 transition-[height] w-full focus-within:h-28 border-b-[3px] md:max-2xl:text-xl text-lg placeholder-mudbrick ring-0 outline-0 text-sandstone focus-within:border-slate border-sandstone border-inherit"/> 
+                                    ? <textarea name="message" value={contactInfo.message} onChange={handleChange} className=" pl-1.5 py-0.5 group bg-shark h-auto duration-700 transition-[height] w-full focus-within:h-28 border-b-[3px] md:text-xl text-lg placeholder-mudbrick ring-0 outline-0 text-sandstone focus-within:border-slate border-sandstone border-inherit"/> 
                                     : <input type="text" name={inputDetails.inputName} value={inputDetails.inputHook} onChange={handleChange} className=" group pl-1 pt-0.5 bg-shark border-b-[3px] md:text-xl text-lg placeholder-mudbrick ring-0 outline-0 text-sandstone visited:autofill:text-sandstone focus-within:border-slate border-sandstone border-inherit w-full"/>
                                 }
                         </label>
