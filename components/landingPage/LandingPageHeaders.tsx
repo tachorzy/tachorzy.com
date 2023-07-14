7c0baabdc191c0108b15f71e67a4ed13345afb51
tachorzy <tachorzyad@gmail.com>
2023-07-14 02:03:10 -0500

    Fixed build error in LandingPageHeaders, special characters in headers

--- components/landingPage/LandingPageHeaders.tsx
+++ components/landingPage/LandingPageHeaders.tsx
@@ -4,7 +4,7 @@ const LandingPageHeaders = () => {
     return(
         <div className={ManropeExtraBold.className}>
             <h1 className=" text-dusty md:text-landingPageTitle text-5xl font-bold md:mt-28 mt-32">Tariq<br/>Achor Zyad</h1>
-            <h2 className="text-sandstone lg:text-2xl md:text-2xl text-landingPageBioMobile font-bold pt-2 lg:mt-5 mt-4 leading-relaxed w-4/5 ">Hey, I'm Tariq, a senior Computer Science student at the University of Houston, <div className="text-dusty">Exploring the vast world of tech.</div></h2>
+            <h2 className="text-sandstone lg:text-2xl md:text-2xl text-landingPageBioMobile font-bold pt-2 lg:mt-5 mt-4 leading-relaxed w-4/5 ">{"Hey, I'm Tariq, a senior Computer Science student at the University of Houston,"} <div className="text-dusty">{"Exploring the vast world of tech."}</div></h2>
         </div>
     );
 }
