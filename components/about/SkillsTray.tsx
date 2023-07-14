d20e053e0aa7034ac8e7ecee86716cbe7b23639f
tachorzy <tachorzyad@gmail.com>
2023-07-14 01:59:18 -0500

    Added key prop to SkillsTray

--- components/about/SkillsTray.tsx
+++ components/about/SkillsTray.tsx
@@ -11,15 +11,13 @@ const SkillsTray = () => {
     return(
         <div className={ManropeRegular.className}>
               <div className="flex flex-row flex-wrap md:mt-1 gap-x-7 self-end pt-3 mb-[8.75rem]">
-                {Array.from(SKILLS.entries()).map(([programmingLanguage, documentation]) => {
+                {Array.from(SKILLS.entries()).map(([programmingLanguage, documentation], index) => {
                     return(
-                        <div>
-                            <div className="flex flex-col group items-center">
-                                <Link href={documentation} target="_blank">
-                                    <Image src ={`/icons/${programmingLanguage}.svg`} width="0" height="0" alt="language/library/framework icon" className="select-none md:max-2xl:w-9 md:h-9 w-6 h-6"></Image>
-                                </Link>
-                                <h1 className="absolute mt-10 opacity-0 group-hover:opacity-100 animation-fade duration-700 text-dusty leading-relaxed pt-2 font-semibold md:text-sm text-xxs">{programmingLanguage}</h1>
-                            </div>
+                        <div key={index} className="flex flex-col group items-center">
+                            <Link href={documentation} target="_blank">
+                                <Image src ={`/icons/${programmingLanguage}.svg`} width="0" height="0" alt="language/library/framework icon" className="select-none md:max-2xl:w-9 md:h-9 w-6 h-6"></Image>
+                            </Link>
+                            <h1 className="absolute mt-10 opacity-0 group-hover:opacity-100 animation-fade duration-700 text-dusty leading-relaxed pt-2 font-semibold md:text-sm text-xxs">{programmingLanguage}</h1>
                         </div>
                 )})}
             </div>
