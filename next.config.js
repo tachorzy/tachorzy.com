256b4f36479d8fe49e0a6eb5040e9825d24b6b11
tachorzy <tachorzyad@gmail.com>
2023-07-15 03:28:20 -0500

    Updated resume

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Reference: https://nextjs.org/docs/api-reference/next.config.js/environment-variables 
+  // for client side env variables
+  env: { 
+    GETFORM_KEY: process.env.GETFORM_KEY,
+  },
 }
 
 module.exports = nextConfig
