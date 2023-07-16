27f792c896fc2ce1d4451856981b4a7fe5ef965a
tachorzy <tachorzyad@gmail.com>
2023-07-15 19:39:38 -0500

    Adjusted nextButton margins props

--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="flex flex-col md:flex-row gap-y-12 md:gap-x-12 flex-wrap"> 
+        <div className="flex flex-col md:flex-row gap-y-12 md:gap-x-12 flex-wrap xl:mb-9"> 
             {Array.from(PROJECTS.entries()).map(([projectName, projectDetails], index) => {
                 return(
                     <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
