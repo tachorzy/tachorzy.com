5db684a28ca1e8ed20f3f1e8bc26be75a58a5301
tachorzy <tachorzyad@gmail.com>
2023-08-09 16:24:39 -0500

    renamed projects map

--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -1,10 +1,10 @@
-import { PROJECTS } from '../../lib/projects'
+import { projects } from '../../lib/projects'
 import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
         <div className="flex flex-col md:flex-row gap-y-12 md:gap-x-12 flex-wrap mb-14 2xl:mb-9"> 
-            {Array.from(PROJECTS.entries()).map(([projectName, projectDetails], index) => {
+            {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
                 return(
                     <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
                 )
