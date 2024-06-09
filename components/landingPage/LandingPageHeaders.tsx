c24c84dd2c854f3d3ac43c65a8c72e61c5e7a130
Tariq A <tachorzyad@gmail.com>
2024-06-08 21:19:05 -0500

    style: adjusted medium sized screen landing page text

--- components/landingPage/LandingPageHeaders.tsx
+++ components/landingPage/LandingPageHeaders.tsx
@@ -3,8 +3,8 @@ import { ManropeExtraBold } from '../../lib/localNextFonts'
 const LandingPageHeaders = () => {
     return(
         <div className={ManropeExtraBold.className}>
-            <h1 className=" text-dusty md:text-landingPageTitle text-5xl font-bold md:mt-28 mt-32">Tariq<br/>Achor Zyad</h1>
-            <h2 className="text-sandstone lg:text-2xl md:text-2xl text-landingPageBioMobile font-bold pt-2 lg:mt-5 mt-4 leading-relaxed w-4/5 ">{"Hey, I'm Tariq, a senior Computer Science student at the University of Houston,"} <div className="text-dusty">{"Exploring the vast world of tech."}</div></h2>
+            <h1 className=" text-dusty md:text-landingPageTitle text-5xl font-bold md:mt-20 lg:mt-28 mt-32">Tariq<br/>Achor Zyad</h1>
+            <h2 className="text-sandstone lg:text-2xl text-landingPageBioMobile font-bold pt-2 lg:mt-5 mt-4 leading-relaxed w-4/5 ">{"Hey, I'm Tariq, a senior Computer Science student at the University of Houston,"} <div className="text-dusty">{"Exploring the vast world of tech."}</div></h2>
         </div>
     );
 }
