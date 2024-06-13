9e87eef9d692119e1528d2600676583a6c81d2be
Tariq A <tachorzyad@gmail.com>
2024-06-12 23:56:47 -0500

    refactor: refactored success page to fix build error

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -13,14 +13,13 @@ export default async function Home() {
                 <div className={ManropeMedium.className + ` h-screen text-center flex flex-col gap-y-3 justify-center content-center self-center md:w-1/2`}>
                     <div className="relative mx-5 self-center">
                         <div className="flex flex-row gap-x-4 items-center">
-                            <h1 className={` text-sandstone lg:xl:text-5xl md:text-4xl text-2xl font-semibold py-4 leading-relaxed select-none`}>Thank You</h1>
+                            <h1 className={`text-sandstone lg:xl:text-5xl md:text-4xl text-2xl font-semibold py-4 leading-relaxed select-none`}>Thank You</h1>
                             <TypeAnimation
                                 className={Quest.className + ` text-sandstone lg:xl:text-5xl md:text-4xl text-2xl py-4 leading-relaxed select-none delay-700`}
                                 sequence={["شكرا بزاف", 1000]}
                                 style={{direction: "rtl"}}
                                 cursor={false}
-                            >
-                            </TypeAnimation>
+                            />
                         </div>
                     </div>
                     <h2 className="text-sandstone lg:xl:text-4xl md:text-3xl text-xl leading-relaxed mx-auto">{"Your message has been submitted!"}<br/>{"I'll make sure to get back to you super soon."}</h2> 
