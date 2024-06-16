75bc48d8fc0bbf5bc10af477200f1d3bbf3af153
Tariq A <tachorzyad@gmail.com>
2024-06-16 04:44:33 -0500

    chore: updated the about me text in the about me section, because this website is all about me, and its 4:44 am and all I got to talk about is about me

--- components/about/AboutMe.tsx
+++ components/about/AboutMe.tsx
@@ -2,9 +2,9 @@ import { JakartaRegular, JakartaMedium } from '../../lib/localNextFonts'
 
 const AboutMe = () => {
     return(
-        <div className={JakartaMedium.className + " text-sandstone lg:text-xl md:text-lg text-xs font-medium pt-1.5 flex flex-col gap-y-4 lg:mt-2 mt-1.5 leading-loose md:w-3/5 w-4/5"}>
-            <p>{"I'm a developer with a tireless passion for full-stack web development. With that passion, I continuously drive myself to learn, grow and create."}</p>
-            <p>{"Over the years, I've built scalable applications, created responsive user interfaces, and enabled user-focused experiences. All of which you can find here on this portfolio."}</p>
+        <div className={JakartaRegular.className + " text-sandstone lg:text-lg md:text-base text-xs font-medium pt-6 flex flex-col gap-y-4 lg:mt-2 mt-1.5 leading-loose md:w-3/5 w-4/5"}>
+            <p>{"I'm an intrepid developer with a tireless passion for software development. With that passion, I continuously drive myself to learn, grow, and create."}</p>
+            <p>{"Over the years, I've built scalable applications, instructed hundreds of students in algorithm design and analysis, led the agile development of production-ready software, and enabled convenient user-focused experiences."}</p>
             <p>The technologies that I use:</p>
         </div>
     );
