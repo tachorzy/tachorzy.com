2e0aca04eb00c4feb44716ce7a43f6d1d93f7969
Tariq A <tachorzyad@gmail.com>
2024-06-16 00:37:15 -0500

    style: changed font in LandingPageHeader to JakartaSans

--- components/landingPage/LandingPageHeaders.tsx
+++ components/landingPage/LandingPageHeaders.tsx
@@ -1,10 +1,10 @@
-import { ManropeExtraBold } from '../../lib/localNextFonts'
+import { JakartaRegular, JakartaMedium, JakartaExtraBold } from '../../lib/localNextFonts'
 
 const LandingPageHeaders = () => {
     return(
-        <div className={ManropeExtraBold.className}>
-            <h1 className=" text-dusty md:text-7xl xl:text-landingPageTitle text-5xl font-bold md:mt-20 lg:mt-28 mt-32">Tariq<br/>Achor Zyad</h1>
-            <h2 className="text-sandstone md:text-xl xl:text-2xl text-landingPageBioMobile font-bold pt-2 lg:mt-5 mt-4 leading-relaxed w-4/5 ">{"Hey, I'm Tariq, a senior Computer Science student at the University of Houston,"} <div className="text-dusty">{"Exploring the vast world of tech."}</div></h2>
+        <div>
+            <h1 className={JakartaExtraBold.className + " text-dusty md:text-7xl xl:text-landingPageTitle text-5xl font-bold md:mt-20 lg:mt-28 mt-32"}>Tariq<br/>Achor Zyad</h1>
+            <h2 className={JakartaMedium.className + " text-sandstone md:text-xl xl:text-2xl text-landingPageBioMobile font-bold pt-2 lg:mt-5 mt-4 leading-relaxed w-4/5"}>{"Hey, I'm Tariq, a senior Computer Science student at the University of Houston—"} <div className="text-dusty">{"exploring the vast world of tech."}</div></h2>
         </div>
     );
 }
--- lib/localNextFonts.ts
+++ lib/localNextFonts.ts
@@ -24,3 +24,38 @@ export const Quest = localFont({
     src: '../public/fonts/Questv1/Questv1-Regular.otf',
     weight: '500'
 })
+
+export const JakartaExtraBold = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-ExtraBold.otf',
+    weight: '500'
+})
+
+export const JakartaBold = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-Bold.otf',
+    weight: '500'
+})
+
+export const JakartaMedium = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-Medium.otf',
+    weight: '500'
+})
+
+export const JakartaRegular = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-Regular.otf',
+    weight: '500'
+})
+
+export const JakartaLight = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-Light.otf',
+    weight: '500'
+})
+
+export const JakartaExtraLight = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-ExtraLight.otf',
+    weight: '500'
+})
+
+export const JakartaSemibold = localFont({
+    src: '../public/fonts/Jakarta/PlusJakartaSans-SemiBold.otf',
+    weight: '500'
+})
