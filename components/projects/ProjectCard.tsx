95f38df0e9204b0fccf05a849659ed7bc8e6d634
Tariq A <tachorzyad@gmail.com>
2024-06-17 03:06:36 -0500

    style: stylized the scroll behavior of the project carousel

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -15,13 +15,13 @@ interface project{
 const ProjectCard = (props: project) => {
     const tempHostLink: string | null = props.hostLink
     return(
-        <div className="mt-0.5 w-[17rem] lg:w-80 lg:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500">
+        <div className="snap-start mt-0.5 w-[17rem] lg:w-80 lg:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500">
             <Link href={ tempHostLink != null ? tempHostLink : props.gitHubLink } target="_blank" className="w-full h-2/3">
                 <Image src={`/projects/${props.imageUrl}`} height="0" width={350} alt="aesthetic gif" className="rounded-t-md h-[56%]"></Image>
             </Link>
-            <div className="justify-center content-center pt-4 px-6 pb-1">
-                <p className={JakartaExtraBold.className + " text-dusty text-2xl"}>{props.name}</p>
-                <p className={JakartaSemibold.className + " text-mudbrick text-lg"}>{props.description}</p>
+            <div className={"justify-center content-center pt-4 px-6 pb-1"}>
+                <p className={JakartaExtraBold.className + " text-dusty text-xl"}>{props.name}</p>
+                <p className={JakartaSemibold.className + " text-mudbrick text-base"}>{props.description}</p>
                 <ProjectTechTray techStack={props.techStack} gitHubLink={props.gitHubLink} hostLink={props.hostLink}/>
             </div>
         </div>
--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="grid md:grid-flow-col gap-y-12 md:gap-x-12 mb-14 2xl:mb-9 overflow-x-scroll scroll-y md:scroll-x"> 
+        <div className="grid md:grid-flow-col gap-y-12 md:gap-x-12 mb-14 2xl:mb-9 md:overflow-x-scroll md:overflow-y-hidden overflow-y-scroll overflow-x-hidden scroll-ml-2 md:scroll-ml-0 scroll-pt-5 snap-x scroll-smooth pb-10"> 
             {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
                 return(
                     <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
