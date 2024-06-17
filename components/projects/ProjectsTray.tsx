14926d2ba65603ba62fb7cc7f6d11ac53635debf
Tariq A <tachorzyad@gmail.com>
2024-06-17 02:05:46 -0500

    style: restyled ProjectTray into a grid (carousel)

--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="flex flex-col md:flex-row gap-y-12 md:gap-x-12 mb-14 2xl:mb-9 overflow-hidden"> 
+        <div className="grid md:grid-flow-col gap-y-12 md:gap-x-12 mb-14 2xl:mb-9 overflow-x-scroll scroll-y md:scroll-x"> 
             {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
                 return(
                     <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
