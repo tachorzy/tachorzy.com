79a508415d31c049366142f31cee55ef15a40360
Tariq A <tachorzyad@gmail.com>
2024-06-19 20:51:29 -0500

    refactor: removed scrollbar/carousel idea from ProjectsTray

--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,15 +3,12 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="relative z-40">
-            <div className="absolute inset-0 z-20 w-1/4 ml-[85%] bg-[linear-gradient(to_left,#1c2324_25%,transparent)]"></div> 
-            <div className="grid md:grid-flow-col lg:gap-y-6 lg:gap-x-0 xl:gap-y-12 md:gap-x-12 mb-14 2xl:mb-9 md:overflow-x-scroll md:overflow-y-hidden overflow-y-scroll overflow-x-hidden snap-x scroll-smooth pb-10"> 
-                {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
-                    return(
-                        <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
-                    )
-                })}
-            </div>
+        <div className="flex md:flex-row flex-wrap lg:gap-y-0 lg:gap-x-0 xl:gap-y-12 md:gap-x-12 mb-14 2xl:mb-9"> 
+            {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
+                return(
+                    <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
+                )
+            })}
         </div>
     );
 }
