82aaef95486a8df4754d51160b12c10d4ad46056
Tariq A <tachorzyad@gmail.com>
2024-06-24 04:31:15 -0500

    style: restyled hamburger menu dropdown and changed the icons

--- components/hamburgerMenu/HamburgerMenu.tsx
+++ components/hamburgerMenu/HamburgerMenu.tsx
@@ -21,7 +21,7 @@ const HamburgerMenu = () => {
         ["about", {text: "About", width: "24", height: "24"}],
         ["projects", {text: "Projects", width: "24", height: "24"}],
         // ["travel", {text: "Travel", width: "27", height: "27"}],
-        ["contact", {text: "Contact", width: "26", height: "26"}],
+        ["contact", {text: "Contact", width: "24", height: "24"}],
     ])
 
     return (
@@ -30,10 +30,10 @@ const HamburgerMenu = () => {
                 <Image src="/icons/hamburgerMenu/hamburger-menu.svg" width="0" height="0" alt="hamburger menu icon" className="cursor-pointer 2xl:w-14 2xl:h-14 lg:w-12 lg:h-12 md:w-10 md:h-10 w-9 h-9"/>
             </button>
             { isActive ? (
-                <div className="w-56 h-58 self-end mr-12 bg-sandstone rounded-2xl fixed right-0 mt-2 shadow-xl" ref={menuRef}>
-                    <div className="flex flex-col text-bark font-bold py-3">
+                <div className="w-64 h-48 self-end mr-12 bg-metal border-2 border-opacity-75 border-[#1C2B2D] rounded-md fixed right-0 mt-2 shadow-xl" ref={menuRef}>
+                    <div className="flex flex-col text-sandstone font-bold py-3">
                         {Array.from(menuOptions.entries()).map(([option, optionDetails], index) => (
-                            <div key={index} className="flex flex-row gap-x-4 items-center hover:bg-dusty pl-6 py-2 select-none" onClick = {() => setIsActive(!isActive)}>      
+                            <div key={index} className="flex flex-row gap-x-4 items-center hover:bg-[#1C2B2D] pl-6 py-2 select-none" onClick = {() => setIsActive(!isActive)}>      
                                 <Link href={option != 'home' ? `#${option}` : '/'} className="w-full flex flex-row gap-x-4 items-center">
                                     <Image src={`/icons/hamburgerMenu/${option}.svg`} width={Number(optionDetails.width)} height={Number(optionDetails.height)} alt="home icon" className=""></Image>
                                     {optionDetails.text}
