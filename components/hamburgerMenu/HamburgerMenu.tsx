28867c35530c7f988ed3dfe152b4e93a44932e9d
Tariq A <tachorzyad@gmail.com>
2024-06-24 04:47:57 -0500

    refactor: removed old lightmode toggle button and the dimensions

--- components/hamburgerMenu/HamburgerMenu.tsx
+++ components/hamburgerMenu/HamburgerMenu.tsx
@@ -1,10 +1,9 @@
-"use client"; //client component
+"use client";
 
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState, useRef } from 'react'
 import { useOnClickOutside } from 'usehooks-ts'
-import LightModeToggle from './LightModeToggle'
 
 const HamburgerMenu = () => {
     const [isActive, setIsActive] = useState(false)
@@ -16,12 +15,12 @@ const HamburgerMenu = () => {
 
     useOnClickOutside(menuRef, handleClickOutside)
 
-    const menuOptions: Map<string, {text: string, width: string, height: string}> = new Map([
-        ["home", {text: "Home", width: "24", height: "24"}],
-        ["about", {text: "About", width: "24", height: "24"}],
-        ["projects", {text: "Projects", width: "24", height: "24"}],
+    const menuOptions: Map<string, {text: string}> = new Map([
+        ["home", {text: "Home"}],
+        ["about", {text: "About"}],
+        ["projects", {text: "Projects"}],
         // ["travel", {text: "Travel", width: "27", height: "27"}],
-        ["contact", {text: "Contact", width: "24", height: "24"}],
+        ["contact", {text: "Contact"}],
     ])
 
     return (
@@ -35,15 +34,11 @@ const HamburgerMenu = () => {
                         {Array.from(menuOptions.entries()).map(([option, optionDetails], index) => (
                             <div key={index} className="flex flex-row gap-x-4 items-center hover:bg-[#1C2B2D] pl-6 py-2 select-none" onClick = {() => setIsActive(!isActive)}>      
                                 <Link href={option != 'home' ? `#${option}` : '/'} className="w-full flex flex-row gap-x-4 items-center">
-                                    <Image src={`/icons/hamburgerMenu/${option}.svg`} width={Number(optionDetails.width)} height={Number(optionDetails.height)} alt="home icon" className=""></Image>
+                                    <Image src={`/icons/hamburgerMenu/${option}.svg`} width={19} height={19} alt="home icon" className=""></Image>
                                     {optionDetails.text}
                                 </Link>
                             </div>
                         ))}
-                        {/* <div className="flex flex-row gap-x-[1.15rem] items-center pl-6 pt-3 pb-2.5 select-none">
-                            <Image src="/fanous.svg" width={36} height={36} alt="" className="pb-3 pr-[0.18rem]"></Image>
-                            <LightModeToggle></LightModeToggle>
-                        </div> */}
                     </div>
                 </div>
             ) : (
