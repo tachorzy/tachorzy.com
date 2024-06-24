212a835bec47d8470ff49a9b8dfc4154482e7ab4
Tariq A <tachorzyad@gmail.com>
2024-06-24 02:09:01 -0500

    feature: added ternary for text size in project cards and changed TensorIoT project details

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -13,15 +13,16 @@ interface project{
 }
 
 const ProjectCard = (props: project) => {
-    const tempHostLink: string | null = props.hostLink
+    const tempHostLink: string | null = props.hostLink 
+    
     return(
         <div className="snap-start w-[17rem] lg:w-80 lg:h-[19.25rem] lg:scale-[80%] lg:hover:scale-[85%] xl:scale-100 bg-metal rounded-md shadow-2xl hover:scale-105 xl:hover:scale-105 duration-500">
             <Link href={ tempHostLink != null ? tempHostLink : props.gitHubLink } target="_blank" className="w-full h-2/3">
                 <Image src={`/projects/${props.imageUrl}`} height={350} width={350} alt="aesthetic gif" className="rounded-t-md h-[56%]"></Image>
             </Link>
             <div className={"justify-center content-center pt-4 px-6 pb-1"}>
-                <p className={JakartaExtraBold.className + " text-dusty text-xl"}>{props.name}</p>
-                <p className={JakartaSemibold.className + " text-mudbrick text-base"}>{props.description}</p>
+                <p className={JakartaExtraBold.className + `${props.name.length > 22 ? " text-lg " : " text-xl "} text-dusty`}>{props.name}</p>
+                <p className={JakartaSemibold.className + ` ${props.description.length > 25 ? " text-sm " : " text-base "} text-mudbrick`}>{props.description}</p>
                 <ProjectTechTray techStack={props.techStack} gitHubLink={props.gitHubLink} hostLink={props.hostLink}/>
             </div>
         </div>
