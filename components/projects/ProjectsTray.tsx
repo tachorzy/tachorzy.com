af4ba03e22ebe121a6d11c81d76aaa29b2561a9c
Tariq A <tachorzyad@gmail.com>
2024-06-23 22:17:48 -0500

    refactor: edited responsive margins on ProjectsTray

--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="flex md:flex-row flex-wrap -mt-3 lg:-ml-7 xl:ml-0 xl:mt-7 lg:gap-y-0 lg:gap-x-0 gap-y-8 xl:gap-y-12 xl:gap-x-12 -mb-48"> 
+        <div className="flex md:flex-row flex-wrap -mt-3 lg:-ml-7 xl:ml-0 xl:mt-7 lg:gap-y-0 lg:gap-x-0 gap-y-8 xl:gap-y-12 xl:gap-x-12 lg:-mb-48 mb-14 2xl:mb-9"> 
             {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
                 return(
                     <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
