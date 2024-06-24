f58a964ce7610c19bbfefb3818b14a50b6da55e5
Tariq A <tachorzyad@gmail.com>
2024-06-24 05:25:18 -0500

    refactor: extracted button into MenuButton component

--- components/hamburgerMenu/HamburgerMenu.tsx
+++ components/hamburgerMenu/HamburgerMenu.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { useState, useRef } from 'react'
 import { useOnClickOutside } from 'usehooks-ts'
 import { motion } from 'framer-motion';
+import MenuButton from './MenuButton'
 
 const HamburgerMenu = () => {
     const [isActive, setIsActive] = useState(false)
@@ -26,9 +27,7 @@ const HamburgerMenu = () => {
 
     return (
         <div>
-            <button className="p-1.5 hover:bg-sandstone duration-700 transition-colors hover:bg-opacity-5 rounded-2xl cursor-pointer select-none border-transparent" onClick = {() => setIsActive(!isActive)}>
-                <Image src="/icons/hamburgerMenu/hamburger-menu.svg" width="0" height="0" alt="hamburger menu icon" className="cursor-pointer 2xl:w-14 2xl:h-14 lg:w-12 lg:h-12 md:w-10 md:h-10 w-9 h-9"/>
-            </button>
+            <MenuButton isActive={isActive} setIsActive={setIsActive}/>
             { isActive ? (
                 <motion.div
                     initial={{ x: '100vw' }}
