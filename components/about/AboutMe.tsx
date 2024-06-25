68399c63400de385b3f5dfbfe77425f06c61d633
Tariq A <tachorzyad@gmail.com>
2024-06-24 23:36:00 -0500

    style: Refined mobile responsiveness in AboutMe and Projects section

--- components/about/AboutMe.tsx
+++ components/about/AboutMe.tsx
@@ -2,7 +2,7 @@ import { JakartaRegular, JakartaMedium } from '../../lib/localNextFonts'
 
 const AboutMe = () => {
     return(
-        <div className={JakartaRegular.className + " text-sandstone lg:text-lg md:text-base text-xs font-medium pt-6 flex flex-col gap-y-4 lg:mt-2 mt-1.5 leading-loose md:w-3/5 w-4/5"}>
+        <div className={JakartaRegular.className + " text-sandstone lg:text-lg md:text-base text-xs font-medium pt-6 flex flex-col gap-y-4 lg:mt-2 mt-1.5 leading-loose md:w-3/5 w-10/12"}>
             <p>{"I'm a developer with a tireless passion for software development. With that passion, I continuously drive myself to learn, grow, and create."}</p>
             <p>{"Over the years, I've built scalable applications, instructed hundreds of students in algorithm design and analysis, led the agile development of production-ready software, and enabled convenient user-focused experiences."}</p>
         </div>
