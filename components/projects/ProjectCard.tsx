bb46de54315d92af0414c8886c886832537db57e
tachorzy <tachorzyad@gmail.com>
2024-09-01 00:33:24 -0500

    style: adjusted project card sizing and project tray gaps

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -16,7 +16,7 @@ const ProjectCard = (props: project) => {
     const tempHostLink: string | null = props.hostLink 
     
     return(
-        <div className="snap-start w-[17rem] lg:w-80 lg:h-[19.25rem] lg:scale-[80%] lg:hover:scale-[85%] xl:scale-100 bg-metal rounded-md hover:scale-105 xl:hover:scale-105 duration-500 border-2 border-opacity-75 border-[#1C2B2D]">
+        <div className="snap-start w-[17rem] lg:w-80 lg:h-[19.25rem] lg:scale-[80%] lg:hover:scale-[85%] xl:scale-[95%] xl:hover:scale-100 bg-metal rounded-md hover:scale-105 duration-500 border-2 border-opacity-75 border-[#1C2B2D]">
             <Link href={ tempHostLink != null ? tempHostLink : props.gitHubLink } target="_blank" className="w-full h-2/3">
                 <Image src={`/projects/${props.imageUrl}`} height={350} width={350} alt="aesthetic gif" className="rounded-t-md h-[56%]"></Image>
             </Link>
--- components/projects/ProjectsTray.tsx
+++ components/projects/ProjectsTray.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 const ProjectsTray = () => {
     return(
-        <div className="flex md:flex-row flex-wrap mt-1 lg:-ml-7 xl:ml-0 xl:mt-7 lg:gap-y-0 lg:gap-x-0 gap-y-8 xl:gap-y-12 xl:gap-x-12 lg:-mb-48 mb-14 2xl:mb-20"> 
+        <div className="flex md:flex-row flex-wrap mt-1 lg:-ml-7 xl:ml-0 xl:mt-7 lg:gap-y-0 lg:gap-x-0 gap-y-8 xl:gap-y-4 xl:gap-x-4 lg:-mb-48 mb-14 2xl:mb-20"> 
             {Array.from(projects.entries()).map(([projectName, projectDetails], index) => {
                 return(
                     <ProjectCard key={index} name={projectName} description={projectDetails.description} imageUrl={projectDetails.imageUrl} techStack={projectDetails.techStack} hostLink={projectDetails.hostLink} gitHubLink={projectDetails.gitHubLink}></ProjectCard>
