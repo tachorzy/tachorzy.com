0dd75ab5b2c4972de4679f69d67377adc9b256c1
tachorzy <tachorzyad@gmail.com>
2024-08-31 19:47:36 -0500

    style: edited margin bottom to logo for 2xl screens

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     return(
         <div className="flex flex-col justify-end">
             <Image src="/ط.svg" width={60} height={60} className="absolute right-0 bottom-0 mr-[3.75rem] mb-12 select-none z-0 lg:hidden block" alt="smaller ط"></Image>
-            <Image src="/ط.svg" width={100} height={100} className="absolute right-0 bottom-0 mr-[3.75rem] mb-12 select-none z-0 hidden md:block 2xl:mb-[17%]" alt="larger ط"></Image>
+            <Image src="/ط.svg" width={100} height={100} className="absolute right-0 bottom-0 mr-[3.75rem] select-none z-0 hidden md:block mb-12 2xl:bottom-48" alt="larger ط"></Image>
         </div>
     );
 }
