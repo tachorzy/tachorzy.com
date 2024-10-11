7005a019059b7a18ac26c8baedafb0c5534603b8
tachorzy <tachorzyad@gmail.com>
2024-10-10 19:56:53 -0500

    feat: added Travel section back with updated CountryCardTray

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import SectionLayout from '../components/section/SectionLayout'
 import SocialsTray from '../components/about/SocialsTray'
 import SkillsTray from '../components/about/SkillsTray'
 import ProjectsTray from '../components/projects/ProjectsTray'
+import CountryCardTray from 'components/travel/CountryCardTray'
 import ContactSection from 'components/contact/ContactSection'
 
 export default function Home() {
@@ -13,8 +14,9 @@ export default function Home() {
   const sections: Map<string, { margin?: string, sectionTitle?: string, sectionTitleMargin?: string, coreComponent: ReactElement, trayComponent?: ReactElement,  pageNumber?: string, nextPageReference?: string, nextButtonMargins?: string }> = new Map([
     ["home", {coreComponent: <LandingPageHeaders/>, trayComponent: <SocialsTray/>, nextPageReference: "#about", nextButtonMargins: "mt-[19rem] lg:mt-[15rem] xl:mt-[21.5rem] 2xl:mt-[21%]"}],
     ["about", { sectionTitle: "About Me", coreComponent: <AboutMe/>, trayComponent: <SkillsTray/>, pageNumber: "١", nextPageReference: "#projects", nextButtonMargins: "mt-2.5 2xl:mt-[11.5%] 2xl:mb-36"}],
-    ["projects", {sectionTitle: "Projects", sectionTitleMargin: "mb-7", coreComponent: <ProjectsTray/>, pageNumber: "٢", nextPageReference: "#contact", nextButtonMargins: "mt-10 lg:mt-72 lg:mb-16 2xl:mt-[36%] 2xl:mb-28"}],
-    ["contact", {margin: "mb-72 mt-[40rem]", sectionTitle: "Get in touch!", sectionTitleMargin: "mb-7", coreComponent: <ContactSection/>, pageNumber: "٣"}
+    ["projects", {sectionTitle: "Projects", sectionTitleMargin: "mb-7", coreComponent: <ProjectsTray/>, pageNumber: "٢", nextPageReference: "#travel", nextButtonMargins: "mt-2.5 lg:mt-72 lg:mb-16 2xl:mt-[36%]"}],
+    ["travel", {margin: "mb-72 mt-[40rem]", sectionTitle: "Travel", sectionTitleMargin: "mb-7", coreComponent: <CountryCardTray/>, pageNumber: "٣", nextButtonMargins: "" ,nextPageReference: "#contact"}],
+    ["contact", {margin: "mb-72 ", sectionTitle: "Get in touch!", sectionTitleMargin: "mb-7", coreComponent: <ContactSection/>, pageNumber: "٤"}
   ]
   ]);
 
--- components/travel/CountryCard.tsx
+++ components/travel/CountryCard.tsx
@@ -10,9 +10,9 @@ interface country{
 
 const CountryCard = (props: country) => {
     return(
-        <div className={ManropeRegular.className + " mt-0.5 w-[17rem] h-96 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500"}>
-            <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="w-full h-2/3">
-                <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-t-md"></Image>
+        <div className={ManropeRegular.className + " mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-2 border-opacity-75 border-[#1C2B2D]"}>
+            <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="">
+                <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-md sepia-[35%] saturate-[120%] hover:sepia-0 transition-all duration-700 contrast-[120%] hover:contrast-100"></Image>
             </Link>
         </div>
     );
