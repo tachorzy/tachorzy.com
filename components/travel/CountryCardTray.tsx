b127b6d03ab38b0981c84ad28c57b3e0af3d68a7
tachorzy <tachorzyad@gmail.com>
2024-10-10 21:15:00 -0500

    style: added annotations to country cards

--- components/travel/CountryCardTray.tsx
+++ components/travel/CountryCardTray.tsx
@@ -9,7 +9,7 @@ const CountryCardTray = () => {
         <div className={JakartaMedium.className + " z-0 relative flex md:flex-row flex-wrap mt-1 lg:-ml-7 xl:ml-0 xl:mt-7 lg:gap-y-0 lg:gap-x-0 gap-y-8 xl:gap-y-4 xl:gap-x-4 lg:-mb-48 mb-14 2xl:mb-0 2xl:w-11/12"}>
           {Array.from(PLACES_TRAVELLED.entries()).map(([countryName, countryDetails], index) => {
               return(
-                <CountryCard key={index} name={countryName} isoCode={countryDetails.isoCode} coverImageURL={countryDetails.coverImageURL}/>
+                <CountryCard key={index} name={countryName} yearVisited={countryDetails.yearVisited} isoCode={countryDetails.isoCode} coverImageURL={countryDetails.coverImageURL}/>
               )
             }
           )}
--- lib/travel.ts
+++ lib/travel.ts
@@ -1,13 +1,13 @@
 
-export const PLACES_TRAVELLED: Map<string, { coverImageURL: string, isoCode: string, destinations: string[]}> = new Map([
-        ["El Salvador", { coverImageURL: "/travel/ElSalvador/country_cover.jpg", isoCode: "SV", destinations: ["San Salvador", "Santa Ana", "Suchitoto"]}],
-        ["The Netherlands", { coverImageURL: "/travel/Netherlands/country_cover.jpg", isoCode: "NL", destinations: ["Amsterdam"]}],
-        ["Greece", { coverImageURL: "/travel/Greece/country_cover.jpg", isoCode: "GR", destinations: ["Athens"] }],
-        ["Palestine",{ coverImageURL: "/travel/Palestine/country_cover.jpg", isoCode: "PS", destinations: ["Jerusalem (Old City)", "Bethlehem", "Battir", "Dheisha", "Ramallah", "Nablus"] }],
-        ["Jordan", { coverImageURL: "/travel/Jordan/country_cover.jpg", isoCode: "JO", destinations: ["Amman", "Wadi Musa"] }],
-        ["Lebanon", { coverImageURL: "/travel/Lebanon/country_cover.jpg", isoCode: "LB", destinations: ["Beirut", "Chouf", "Aannaya", "Batroun", "Byblos", "Tripoli", "Bcharre"] }],
-        ["Mexico", { coverImageURL: "/travel/Mexico/country_cover.jpg", isoCode: "MX", destinations: ["CDMX", "Taxco"] }],
-        ["Türkiye", { coverImageURL: "/travel/Turkey/country_cover.jpg", isoCode: "TR", destinations: ["İstanbul", "Urfa", "Diyarbakır", "Mardin"] }],
-        ["Iraq", { coverImageURL: "/travel/Iraq/country_cover.jpg", isoCode: "IQ", destinations: ["Baghdad", "Erbil"] }],
-        ["Egypt", { coverImageURL: "/travel/Egypt/country_cover.jpg", isoCode: "EG", destinations: ["Cairo"] }]
+export const PLACES_TRAVELLED: Map<string, { coverImageURL: string, yearVisited: string, isoCode: string, destinations: string[]}> = new Map([
+        ["El Salvador", { coverImageURL: "/travel/ElSalvador/country_cover.jpg", yearVisited: "2024", isoCode: "SV", destinations: ["San Salvador", "Santa Ana", "Suchitoto"]}],
+        ["The Netherlands", { coverImageURL: "/travel/Netherlands/country_cover.jpg", yearVisited: "2023", isoCode: "NL", destinations: ["Amsterdam"]}],
+        ["Greece", { coverImageURL: "/travel/Greece/country_cover.jpg", isoCode: "GR", yearVisited: "2023", destinations: ["Athens"] }],
+        ["Jerusalem & the West Bank",{ coverImageURL: "/travel/Palestine/country_cover.jpg", yearVisited: "2023", isoCode: "PS", destinations: ["Jerusalem (Old City)", "Bethlehem", "Battir", "Dheisha", "Ramallah", "Nablus"] }],
+        ["Jordan", { coverImageURL: "/travel/Jordan/country_cover.jpg", isoCode: "JO", yearVisited: "2023", destinations: ["Amman", "Wadi Musa"] }],
+        ["Lebanon", { coverImageURL: "/travel/Lebanon/country_cover.jpg", isoCode: "LB", yearVisited: "2023", destinations: ["Beirut", "Chouf", "Aannaya", "Batroun", "Byblos", "Tripoli", "Bcharre"] }],
+        ["Mexico", { coverImageURL: "/travel/Mexico/country_cover.jpg", isoCode: "MX", yearVisited: "2022", destinations: ["CDMX", "Taxco"] }],
+        ["Türkiye", { coverImageURL: "/travel/Turkey/country_cover.jpg", isoCode: "TR", yearVisited: "2022", destinations: ["İstanbul", "Urfa", "Diyarbakır", "Mardin"] }],
+        ["Iraq", { coverImageURL: "/travel/Iraq/country_cover.jpg", isoCode: "IQ", yearVisited: "2022", destinations: ["Baghdad", "Erbil"] }],
+        ["Egypt", { coverImageURL: "/travel/Egypt/country_cover.jpg", isoCode: "EG", yearVisited: "2022", destinations: ["Cairo"] }]
 ]);
