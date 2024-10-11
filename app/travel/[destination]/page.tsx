0caab09849d8436fd5b96489c1f270e2a37e8f97
tachorzy <tachorzyad@gmail.com>
2024-10-11 10:15:53 -0500

    feat: added DestinationCard component

--- app/travel/[destination]/page.tsx
+++ app/travel/[destination]/page.tsx
@@ -9,7 +9,7 @@ export default function Page({
     return (
         <main className="bg-shark flex flex-col h-screen overflow-hidden 2xl:gap-y-24">
             <div>
-
+                
             </div>
         </main>
     )
