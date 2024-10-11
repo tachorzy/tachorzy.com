ff941e22d751e3ef372d2900f012470f34beeb16
tachorzy <tachorzyad@gmail.com>
2024-10-10 20:53:26 -0500

    feat: added El Salvador to travels

--- components/travel/CountryCard.tsx
+++ components/travel/CountryCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { ManropeExtraBold, ManropeMedium, ManropeRegular } from '../../lib/localNextFonts'
+import { JakartaExtraBold } from '../../lib/localNextFonts'
 
 interface country{
     name: string;
@@ -10,9 +10,10 @@ interface country{
 
 const CountryCard = (props: country) => {
     return(
-        <div className={ManropeRegular.className + " mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-2 border-opacity-75 border-[#1C2B2D]"}>
-            <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="">
-                <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-md sepia-[35%] saturate-[120%] hover:sepia-0 transition-all duration-700 contrast-[120%] hover:contrast-100"></Image>
+        <div className={JakartaExtraBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D]`}>
+            <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="relative flex flex-row items-center justify-center text-[invert(0)]">
+                <h1 className={"text-2xl text-bold absolute text-center z-50"}>{props.name}</h1>
+                <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-md sepia-[100%] saturate-[120%] hover:sepia-0 transition-all duration-700 contrast-[120%]"></Image>
             </Link>
         </div>
     );
--- lib/travel.ts
+++ lib/travel.ts
@@ -1,13 +1,13 @@
 
-export const PLACES_TRAVELLED: Map<string, { coverImageURL: string, isoCode: string, destinations: string[] }> = new Map([
-        ["The Netherlands", { coverImageURL: "/travel/Netherlands/country_cover.jpg", isoCode: "NL", destinations: ["Amsterdam"] }],
+export const PLACES_TRAVELLED: Map<string, { coverImageURL: string, isoCode: string, destinations: string[]}> = new Map([
+        ["El Salvador", { coverImageURL: "/travel/ElSalvador/country_cover.jpg", isoCode: "SV", destinations: ["San Salvador", "Santa Ana", "Suchitoto"]}],
+        ["The Netherlands", { coverImageURL: "/travel/Netherlands/country_cover.jpg", isoCode: "NL", destinations: ["Amsterdam"]}],
         ["Greece", { coverImageURL: "/travel/Greece/country_cover.jpg", isoCode: "GR", destinations: ["Athens"] }],
-        ["Palestine",{ coverImageURL: "/travel/Palestine/country_cover.jpg", isoCode: "PS", destinations: ["Jerusalem", "Bethlehem", "Battir", "Dheisha", "Ramallah", "Nablus"] }],
+        ["Palestine",{ coverImageURL: "/travel/Palestine/country_cover.jpg", isoCode: "PS", destinations: ["Jerusalem (Old City)", "Bethlehem", "Battir", "Dheisha", "Ramallah", "Nablus"] }],
         ["Jordan", { coverImageURL: "/travel/Jordan/country_cover.jpg", isoCode: "JO", destinations: ["Amman", "Wadi Musa"] }],
         ["Lebanon", { coverImageURL: "/travel/Lebanon/country_cover.jpg", isoCode: "LB", destinations: ["Beirut", "Chouf", "Aannaya", "Batroun", "Byblos", "Tripoli", "Bcharre"] }],
         ["Mexico", { coverImageURL: "/travel/Mexico/country_cover.jpg", isoCode: "MX", destinations: ["CDMX", "Taxco"] }],
         ["Türkiye", { coverImageURL: "/travel/Turkey/country_cover.jpg", isoCode: "TR", destinations: ["İstanbul", "Urfa", "Diyarbakır", "Mardin"] }],
         ["Iraq", { coverImageURL: "/travel/Iraq/country_cover.jpg", isoCode: "IQ", destinations: ["Baghdad", "Erbil"] }],
-        ["Egypt", { coverImageURL: "/travel/Egypt/country_cover.jpg", isoCode: "EG", destinations: ["Cairo"] }
-    ]
+        ["Egypt", { coverImageURL: "/travel/Egypt/country_cover.jpg", isoCode: "EG", destinations: ["Cairo"] }]
 ]);
