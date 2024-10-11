50503524cc1f6554166347bc6cd4c83d44df9229
tachorzy <tachorzyad@gmail.com>
2024-10-11 00:12:08 -0500

    style: adjusted vignette gradient

--- components/travel/CountryCard.tsx
+++ components/travel/CountryCard.tsx
@@ -11,7 +11,7 @@ interface country{
 
 const CountryCard = (props: country) => {
     return(
-        <div className={JakartaBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D]`}>
+        <div className={JakartaBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D] transition-all duration-500`}>
             <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="relative flex flex-row items-end justify-center text-[invert(0)]">
                 <div className="absolute flex flex-col pb-2 w-2/3">
                     <h1 className={"text-xs text-dune font-bold uppercase tracking-wider text-center z-50"}>{props.name}</h1>
