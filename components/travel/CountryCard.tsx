a41c0f20f3efc6d656e320cddaa6a8132159d6ba
tachorzy <tachorzyad@gmail.com>
2024-10-10 23:12:55 -0500

    style: added vignette effect to travel cards

--- components/travel/CountryCard.tsx
+++ components/travel/CountryCard.tsx
@@ -13,11 +13,12 @@ const CountryCard = (props: country) => {
     return(
         <div className={JakartaBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D]`}>
             <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="relative flex flex-row items-end justify-center text-[invert(0)]">
-                <div className="absolute flex flex-col pb-2">
+                <div className="absolute flex flex-col pb-2 w-2/3">
                     <h1 className={"text-xs font-bold uppercase tracking-wider text-center z-50"}>{props.name}</h1>
                     <h2 className={"text-xs font-bold uppercase tracking-wider text-center z-50"}>{props.yearVisited}</h2>
                 </div>
-                <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-md sepia-[100%] saturate-[120%] hover:sepia-0 transition-all duration-700 contrast-[120%]"></Image>
+                <Image src={"/travel/vignette.svg"} height={700} width={700} alt="vignette" className="w-full h-1/2 z-30 absolute bottom-0 "/>
+                <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-md saturate-[120%] hover:sepia-0 transition-all duration-700 contrast-[120%]"></Image>
             </Link>
         </div>
     );
