3167d6c79a89e90c5bfdaf9b144a10e77fa98f8e
tachorzy <tachorzyad@gmail.com>
2024-10-11 16:08:19 -0500

    feat: added dynamic segment fetching for routes

--- app/travel/[destination]/page.tsx
+++ app/travel/[destination]/page.tsx
@@ -1,15 +1,15 @@
-export default function Page({
-    params,
-}: {
-    params: { country: string; };
-}) {
+import DestinationCardTray from 'components/travel/DestinationCardTray';
 
-    const country = params;
+export default function Page({ params } : {
+    params: { destination: string }
+}){ 
+    const decodedDestination = decodeURIComponent(params.destination);
 
     return (
         <main className="bg-shark flex flex-col h-screen overflow-hidden 2xl:gap-y-24">
-            <div>
-                
+            <div className="flex flex-col items-center">
+                <h1 className="text-4xl text-bold text-dune">{decodedDestination}</h1>
+                <DestinationCardTray countryName={decodedDestination}/>
             </div>
         </main>
     )
--- components/travel/CountryCard.tsx
+++ components/travel/CountryCard.tsx
@@ -10,9 +10,10 @@ interface country{
 }
 
 const CountryCard = (props: country) => {
+
     return(
         <div className={JakartaBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D] transition-all duration-500`}>
-            <Link href={`\\travel\\${props.name}\\country_cover.jpg`} target="_blank" className="relative flex flex-row items-end justify-center text-[invert(0)]">
+            <Link href={`\\travel\\${props.name}`} className="relative flex flex-row items-end justify-center text-[invert(0)]">
                 <div className="absolute flex flex-col pb-2 w-2/3">
                     <h1 className={"text-xs text-dune font-bold uppercase tracking-wider text-center z-50"}>{props.name}</h1>
                     <h2 className={"text-xs text-dune font-bold uppercase tracking-wider text-center z-50"}>{props.yearVisited}</h2>
