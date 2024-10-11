c7a3c3941f01b0e386672fdc75482e404237eec2
tachorzy <tachorzyad@gmail.com>
2024-10-11 01:57:26 -0500

    feat: added dynamic route for travel destinations

---
+++ app/travel/[destination]/page.tsx
@@ -0,0 +1,17 @@
+export default function Page({
+    params,
+}: {
+    params: { country: string; };
+}) {
+
+    const country = params;
+
+    return (
+        <main className="bg-shark flex flex-col h-screen overflow-hidden 2xl:gap-y-24">
+            <div>
+
+            </div>
+        </main>
+    )
+
+};
