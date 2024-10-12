c4ae134728cb75b18eeef8cfe936a5c9a62ea603
tachorzy <tachorzyad@gmail.com>
2024-10-12 09:22:25 -0500

    feat: fixed HamburgerMenu links for MPA

--- components/hamburgerMenu/Dropdown.tsx
+++ components/hamburgerMenu/Dropdown.tsx
@@ -29,7 +29,7 @@ const Dropdown = (props: {isActive: boolean, setIsActive: React.Dispatch<React.S
             <div className="flex flex-col text-sandstone font-bold py-3">
                 {Array.from(menuOptions.entries()).map(([option, optionDetails], index) => (
                     <div key={index} className="flex flex-row gap-x-4 items-center transition-colors hover:bg-[#1C2B2D] pl-6 py-2 select-none" onClick = {() => setIsActive(!isActive)}>      
-                        <Link href={option != 'home' ? `#${option}` : '/'} className="w-full flex flex-row gap-x-4 items-center">
+                        <Link href={option != 'home' ? `/#${option}` : '/'} className="w-full flex flex-row gap-x-4 items-center">
                             <Image src={`/icons/hamburgerMenu/${option}.svg`} width={19} height={19} alt="home icon" className=""></Image>
                             {optionDetails.text}
                         </Link>
