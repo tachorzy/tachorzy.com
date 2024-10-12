d5aa5490ee80f3f76ef72a26a880ad94da40f9dc
tachorzy <tachorzyad@gmail.com>
2024-10-12 16:14:00 -0500

    fix: fixed coverImageURLs for renamed folders

--- lib/travel.ts
+++ lib/travel.ts
@@ -7,7 +7,7 @@ export const PLACES_TRAVELLED: Map<string, { coverImageURL: string, yearVisited:
         ["Jordan", { coverImageURL: "/travel/Jordan/country_cover.jpg", isoCode: "JO", yearVisited: "2023", destinations: ["Amman", "Wadi Musa"] }],
         ["Lebanon", { coverImageURL: "/travel/Lebanon/country_cover.jpg", isoCode: "LB", yearVisited: "2023", destinations: ["Beirut", "Tripoli", "Bcharre", "Chouf", "Aannaya", "Batroun", "Byblos"] }],
         ["Mexico", { coverImageURL: "/travel/Mexico/country_cover.jpg", isoCode: "MX", yearVisited: "2022", destinations: ["CDMX", "Teotihuacan", "Taxco"] }],
-        ["Türkiye", { coverImageURL: "/travel/Turkey/country_cover.jpg", isoCode: "TR", yearVisited: "2022", destinations: ["İstanbul", "Diyarbakır", "Mardin"] }],
+        ["Türkiye", { coverImageURL: "/travel/Türkiye/country_cover.jpg", isoCode: "TR", yearVisited: "2022", destinations: ["İstanbul", "Diyarbakır", "Mardin"] }],
         ["Iraq", { coverImageURL: "/travel/Iraq/country_cover.jpg", isoCode: "IQ", yearVisited: "2022", destinations: ["Baghdad", "Erbil"] }],
         ["Egypt", { coverImageURL: "/travel/Egypt/country_cover.jpg", isoCode: "EG", yearVisited: "2022", destinations: ["Cairo"] }]
 ]);
