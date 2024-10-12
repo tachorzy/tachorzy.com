208b01254e6d666591aada0a8f8c996dad31a86e
tachorzy <tachorzyad@gmail.com>
2024-10-12 11:46:53 -0500

    feat: added gallery photos for El Salvador and Netherlands

--- lib/travel.ts
+++ lib/travel.ts
@@ -1,6 +1,6 @@
 
 export const PLACES_TRAVELLED: Map<string, { coverImageURL: string, yearVisited: string, isoCode: string, destinations: string[]}> = new Map([
-        ["El Salvador", { coverImageURL: "/travel/ElSalvador/country_cover.jpg", yearVisited: "2024", isoCode: "SV", destinations: ["San Salvador", "Santa Ana", "Suchitoto"]}],
+        ["El Salvador", { coverImageURL: "/travel/ElSalvador/country_cover.jpg", yearVisited: "2024", isoCode: "SV", destinations: ["San Salvador", "Santa Ana", "Cerro Verde", "Ruta de las Flores", "Suchitoto", "Cinquera", "La Libertad"]}],
         ["The Netherlands", { coverImageURL: "/travel/Netherlands/country_cover.jpg", yearVisited: "2023", isoCode: "NL", destinations: ["Amsterdam"]}],
         ["Greece", { coverImageURL: "/travel/Greece/country_cover.jpg", isoCode: "GR", yearVisited: "2023", destinations: ["Athens"] }],
         ["Jerusalem & the West Bank",{ coverImageURL: "/travel/Palestine/country_cover.jpg", yearVisited: "2023", isoCode: "PS", destinations: ["Jerusalem", "Bethlehem", "Battir", "Ramallah", "Nablus"] }],
