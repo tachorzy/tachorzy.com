c80920f4642c4a3f04a29d18a67706b3ac2e7c11
tachorzy <tachorzyad@gmail.com>
2024-10-12 21:21:35 -0500

    refactor: removed image fetching

--- components/travel/Gallery.tsx
+++ components/travel/Gallery.tsx
@@ -1,31 +1,39 @@
-import fs from 'fs';
-import path from 'path';
+"use client";
+
+import { useEffect, useState } from 'react';
 import { JakartaMedium } from '../../lib/localNextFonts';
 import dynamic from 'next/dynamic'
+import { supabase } from '../../supabaseClient';
 
 const GalleryImage = dynamic(() => import('components/travel/GalleryImage'), {
     loading: () => <p className="text-white font-bold text-9xl">Loading...</p>,
 })
 
 
-const Gallery = (props: { countryName: string, cityName: string, imagePaths: string[] }) => {
+const Gallery = (props: { countryName: string, cityName: string }) => {
     const { countryName, cityName } = props;
-    let imagePaths = props.imagePaths;
 
-    const imagesDirectory = path.join(process.cwd(), `public/travel/${countryName}/${cityName}`);
+    useEffect(() => {
+        getAllImagesFromBucket()
+      }, [])
 
-    if (fs.existsSync(imagesDirectory)) {
-        const filenames = fs.readdirSync(imagesDirectory);
-        imagePaths = filenames
-            .filter((filename) => /\.(jpg|jpeg|png)$/.test(filename))
-            .map((filename) => `/travel/${countryName}/${cityName}/${filename}`);
-    }
+    const [photoUrls, setPhotoUrls] = useState<Object>([]);
+    async function getAllImagesFromBucket() {
+        const data = await supabase.storage
+          .from("travel-photos")
+          .list(`${countryName}/${cityName}/`);
+        setPhotoUrls(data);        
+      }
     
+
+    console.log(`photoUrls: ${photoUrls}`);
+
+    const photoMap = new Map(Object.entries(photoUrls));
     return (
         <div className={JakartaMedium.className + " z-0 relative flex md:flex-row flex-wrap mt-1 lg:-ml-7 xl:ml-0 xl:mt-7 lg:gap-y-0 lg:gap-x-0 gap-y-8 xl:gap-y-4 xl:gap-x-4 lg:-mb-48 mb-14 2xl:mb-0 2xl:w-11/12"}>
-            {imagePaths.map((src, index) => (
+            {Array.from(photoMap).map((src, index) => (
                 <div key={index}>
-                    <GalleryImage index={index} imageSrc={src} />
+                    <GalleryImage index={index} imageSrc={src[index]} />
                 </div>
             ))}
         </div>
--- components/travel/GalleryImage.tsx
+++ components/travel/GalleryImage.tsx
@@ -9,6 +9,9 @@ const GalleryImage = (props: {index: number, imageSrc: string}) => {
     const [loadingStyle, setLoadingStyle] = useState("opacity-50 animate-pulse duration-700");
 
     const { index, imageSrc } = props;
+    
+
+    console.log(`imageSrc: ${imageSrc}`);
 
     useEffect(() => {
         const fetchImageUrls = async () => {
@@ -22,12 +25,14 @@ const GalleryImage = (props: {index: number, imageSrc: string}) => {
             const urls = data.map((file) => {
                 return supabase.storage.from("travel-photos").getPublicUrl(imageSrc).data.publicUrl;
             });
-
+            console.log(`urls: ${urls}`);
             setImageUrls(urls);
         };
 
         fetchImageUrls();
     }, [imageSrc]);
+
+    console.log(`imageUrls: ${imageUrls}`);
 
     return(
             <div key={index} className="relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D] transition-all duration-500">
