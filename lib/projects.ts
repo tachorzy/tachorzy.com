9959c33093bbeb08f87b8450116aec49715c7859
tachorzy <tachorzyad@gmail.com>
2024-10-15 00:33:25 -0500

    refactor: updated skills in projects (added Supabase to portfolio website and added Go to Gibraltr

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,7 +1,7 @@
 export const projects: Map<string, {description: string, imageUrl: string, techStack: string[], hostLink:string | null, gitHubLink: string}> = new Map([
-    ["Gibraltr", {description: "Visa Search Engine", imageUrl: "aesthetic_station.gif", techStack: ["Next.js", "JavaScript", "TailwindCSS"], hostLink: "https://www.gibraltr.com/", gitHubLink: "https://github.com/tachorzy/Gibraltr"}],
+    ["Gibraltr", {description: "Visa Search Engine", imageUrl: "aesthetic_station.gif", techStack: ["Next.js", "JavaScript", "TailwindCSS", "Go"], hostLink: "https://www.gibraltr.com/", gitHubLink: "https://github.com/tachorzy/Gibraltr"}],
     ["Battuta.ai", {description: "AI Itinerary Generator", imageUrl: "aesthetic_travel.gif", techStack: ["Next.js", "TypeScript", "TailwindCSS"], hostLink: null, gitHubLink: "https://github.com/tachorzy/battuta.ai"}],
     ["TensorIoT Factory Simulator", { description: "End-to-End Manufacturing Simulator", imageUrl: "aesthetic_ships.gif", techStack: ["Next.js", "TypeScript", "TailwindCSS", "Go", "AWS"], hostLink: null, gitHubLink: "https://github.com/tachorzy/Team-WDD-End-to-End-Manufacturing-Simulator"}],
     ["WhichSideOfTheRoad?", { description: "Bidirectional traffic simulator", imageUrl: "aesthetic_traffic.webp", techStack: ["Vue", "TypeScript", "TailwindCSS"], hostLink: "https://what-side-of-the-road.vercel.app/", gitHubLink: "https://github.com/tachorzy/Which-Side-Of-The-Road"}],
-    ["Tariq Achor Zyad", {description: "This Portfolio", imageUrl: "aesthetic_art.gif", techStack: ["Next.js", "TypeScript", "TailwindCSS"], hostLink: null, gitHubLink: "https://github.com/tachorzy/tachorzy.com"}]
+    ["Tariq Achor Zyad", {description: "This Portfolio", imageUrl: "aesthetic_art.gif", techStack: ["Next.js", "TypeScript", "TailwindCSS", "Supabase"], hostLink: null, gitHubLink: "https://github.com/tachorzy/tachorzy.com"}]
 ]);
