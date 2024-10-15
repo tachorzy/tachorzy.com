9cb8502219f27ab262abb4a35bb618a2850ca202
tachorzy <tachorzyad@gmail.com>
2024-10-14 20:08:43 -0500

    refactor: reintroduced loading state into GalleryImage

--- components/travel/gallery/GalleryImage.tsx
+++ components/travel/gallery/GalleryImage.tsx
@@ -2,10 +2,16 @@
 
 import Image from 'next/image'
 import { useState } from 'react';
+
+function cn(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
 const GalleryImage = (props: {index: number, imageSrc: string}) => {
     const { index, imageSrc } = props;
 
-    const [loadingStyle, setLoadingStyle] = useState("opacity-50 animate-pulse duration-700");
+    const [isLoading, setLoading] = useState(true)
 
     return (
             <Image 
@@ -13,10 +19,9 @@ const GalleryImage = (props: {index: number, imageSrc: string}) => {
                 height={150} 
                 width={350} 
                 alt={`travel pic ${index}`} 
-                className={`rounded-md saturate-[120%] transition-all duration-700 contrast-[120%] ${loadingStyle}`}
-                onLoad={() => {setLoadingStyle("opacity-50 animate-pulse duration-300")}}
-                onLoadingComplete={() => {setLoadingStyle("")}}
-                loading="lazy"  
+                className={cn('rounded-md saturate-[120%] transition-all duration-700 contrast-[120%] ', isLoading ? 'opacity-50 animate-pulse' : 'opacity-100')}
+                onLoadingComplete={() => {setLoading(false)}}
+                loading="lazy"
             />
     );
 }
