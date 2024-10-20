5981a88b8ad749c2aaac3f3723341983afdf6822
tachorzy <tachorzyad@gmail.com>
2024-10-20 01:01:54 -0500

    style: readjusted margins on nextPageMargin and adjusted Logo responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const sections: Map<string, Section> = new Map([
     ["home", {coreComponent: <LandingPageHeaders/>, trayComponent: <SocialsTray/>, nextPageReference: "#about", nextButtonMargins: "lg:mt-[15rem] xl:mt-[21.5rem] 2xl:mt-[18.55%]"}],
     ["about", { sectionTitle: "About Me", coreComponent: <AboutMe/>, pageNumber: "١", nextPageReference: "#projects", nextButtonMargins: "mt-2.5 2xl:mt-[6.5%] "}],
     ["projects", {sectionTitle: "Projects", sectionTitleMargin: "mb-7", coreComponent: <ProjectsTray/>, pageNumber: "٢", nextPageReference: "#travel", nextButtonMargins: "mt-2.5 lg:mt-72 2xl:mt-[36%]"}],
-    ["travel", {margin: "mb-72 mt-[40rem]", sectionTitle: "Travel", sectionTitleMargin: "mb-7", coreComponent: <CountryCardTray/>, pageNumber: "٣", nextButtonMargins: "" ,nextPageReference: "#contact"}],
+    ["travel", {margin: "mb-72 mt-[40rem]", sectionTitle: "Travel", sectionTitleMargin: "mb-7", coreComponent: <CountryCardTray/>, pageNumber: "٣", nextButtonMargins: "2xl:mt-3.5" ,nextPageReference: "#contact"}],
     ["contact", {margin: "mb-72 ", sectionTitle: "Get in touch!", sectionTitleMargin: "mb-7", coreComponent: <ContactSection/>, pageNumber: "٤"}
   ]
   ]);
--- components/NextPageButton.tsx
+++ components/NextPageButton.tsx
@@ -9,8 +9,8 @@ interface buttonProps{
 const NextPageButton = (props: buttonProps) => {
     return(
         <div className={`${props.margin} hidden md:block relative bottom-0 opacity-0 hover:opacity-100 w-full h-2 animation-fade duration-700`}>
-            <button className="flex flex-row items-center justify-center absolute bottom-0 inset-x-[42%] mb-12 p-2 bg-metal rounded-full w-[6%] border-2 border-opacity-75 border-[#1C2B2D] cursor-pointer select-none">
-              <Link href={props.nextPageReference}>
+            <button className="absolute bottom-0 inset-x-[42%] mb-12 p-2 bg-metal rounded-full w-[6%] border-2 border-opacity-75 border-[#1C2B2D] cursor-pointer select-none">
+              <Link href={props.nextPageReference} className="w-full flex flex-row items-center justify-center ">
                 <Image src="/icons/chevron.svg" width={24} height={24} alt="chevron icon" className="cursor-pointer"/>
               </Link>
             </button>
