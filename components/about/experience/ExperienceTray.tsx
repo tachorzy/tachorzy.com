0c02af31b6ecafe5c36558a62ac1d902fdff2d48
tachorzy <tachorzyad@gmail.com>
2024-10-19 21:58:59 -0500

    feat: added ExperienceTray component

---
+++ components/about/experience/ExperienceTray.tsx
@@ -0,0 +1,12 @@
+import { JakartaRegular, JakartaMedium } from '../../../lib/localNextFonts'
+
+const ExperienceTray = () => {
+    return(
+        <div className={JakartaRegular.className + " text-sandstone 2xl:text-xl lg:text-lg md:text-base text-xs font-medium pt-6 w-10/12 md:w-1/2 flex flex-col gap-y-4 lg:mt-2 mt-1.5"}>
+
+
+        </div>
+    );
+}
+
+export default ExperienceTray;
