cbc94054d57e994b16eb7bd37f1b3a4fb9b08f2d
tachorzy <tachorzyad@gmail.com>
2024-10-19 22:49:22 -0500

    feat: added props to Experience

--- components/about/experience/Experience.tsx
+++ components/about/experience/Experience.tsx
@@ -11,11 +11,11 @@ const Experience = (props: ExperienceData) => {
     const { date, title, description } = props;
 
     return(
-        <div className={JakartaRegular.className + " text-sandstone 2xl:text-xl lg:text-lg md:text-base text-xs lg:h-24 bg-metal hover:bg-[#1C2B2D] border-l-4 rounded-r-md hover:scale-[101.5%] duration-500 border-opacity-75 border-sandstone border-[#1C2B2D]"}>
+        <div className={JakartaRegular.className + " text-sandstone 2xl:text-xl lg:text-lg md:text-base text-xs w-full lg:h-44 bg-metal hover:bg-[#1C2B2D] border-l-4 duration-500 border-opacity-75 border-sandstone border-[#1C2B2D]"}>
             <div className="flex flex-col gap-y-2 p-4">
                 <p className={JakartaMedium.className + " text-xs text-dusty"}>{date}</p>
                 <p className={JakartaMedium.className + " text-base font-bold text-dusty"}>{title}</p>
-                <p className={JakartaRegular.className + " text-sm text-mudbrick"}>{description}</p>
+                <p className={JakartaRegular.className + " text-sm text-mudbrick leading-relaxed"}>{description}</p>
             </div>
         </div>
     );
--- components/about/experience/ExperienceTray.tsx
+++ components/about/experience/ExperienceTray.tsx
@@ -1,10 +1,19 @@
 import { JakartaRegular, JakartaMedium } from '../../../lib/localNextFonts'
 import Experience from './Experience'
 
+const MapExperience = new Map<string, {date: string, title: string, description: string}>([
+    ["TA", {date: "August 2022 - Present", title: "Undergraduate Teaching Assistant @ UH", description: "Instructed over 1000 students across in topics ranging from OOP to Algorithms & Data Structures in four different courses. And made plenty of friends along the way!"}],
+    ["CougarCS", {date: "August 2023 - May 2024", title: "Director of Tutoring @ CougarCS", description: "Revitalized the tutoring program for UH's largest computer science (CS) organization, increased member retention by 50%, and created the first ever exam review workshops in UH CS."}],
+]);
+
 const ExperienceTray = () => {
     return(
-        <div className={JakartaRegular.className + " text-sandstone 2xl:text-xl lg:text-lg md:text-base text-xs font-medium pt-6 w-10/12 md:w-1/2 flex flex-col lg:mt-2 mt-1.5"}>
-            <Experience/>
+        <div className={JakartaRegular.className + " text-sandstone 2xl:text-xl lg:text-lg md:text-base text-xs font-medium rounded-r-md pt-6 w-10/12 md:w-1/2 flex flex-col lg:mt-2 mt-1.5"}>
+            {Array.from(MapExperience.entries()).map(([experienceKey, experienceDetails]) => {
+                return(
+                    <Experience key={experienceKey} date={experienceDetails.date} title={experienceDetails.title} description={experienceDetails.description}/>
+                )
+            })}
         </div>
     );
 }
