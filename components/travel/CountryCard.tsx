541c187cbbe387d496a8d270a78dfc1daf2315c4
tachorzy <tachorzyad@gmail.com>
2024-11-03 16:27:56 -0600

    style: adjusted padding to CountryCard and added overflow-hidden attribute

--- components/travel/CountryCard.tsx
+++ components/travel/CountryCard.tsx
@@ -12,13 +12,13 @@ interface country{
 const CountryCard = (props: country) => {
 
     return(
-        <div className={JakartaBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D] transition-all duration-500`}>
-            <Link href={`\\travel\\${props.name}`} className="relative flex flex-row items-end justify-center text-[invert(0)]">
-                <div className="absolute flex flex-col pb-2 w-2/3">
+        <div className={JakartaBold.className + ` relative mt-0.5 w-56 md:max-2xl:w-80 md:max-2xl:h-[19.25rem] bg-metal rounded-md shadow-2xl hover:scale-105 duration-500 border-[2.5px] border-opacity-75 border-[#1C2B2D] transition-all duration-500 overflow-hidden`}>
+            <Link href={`\\travel\\${props.name}`} className="relative flex flex-row items-end justify-center text-[invert(0)] overflow-hidden">
+                <div className="absolute flex flex-col pb-2.5 md:pb-7 xl:pb-2 w-2/3">
                     <h1 className={"text-xs text-dune font-bold uppercase tracking-wider text-center z-50"}>{props.name}</h1>
                     <h2 className={"text-xs text-dune font-bold uppercase tracking-wider text-center z-50"}>{props.yearVisited}</h2>
                 </div>
-                <Image src={"/travel/vignette.svg"} height={1300} width={1300} alt="vignette" className="w-full z-30 absolute bottom-0 "/>
+                <Image src={"/travel/vignette.svg"} height={1300} width={1300} alt="vignette" className="w-full z-30 absolute bottom-0"/>
                 <Image src={props.coverImageURL} height={150} width={350} alt={`travel pic from ${props.name}`} className="rounded-md saturate-[120%] hover:sepia-0 transition-all duration-700 contrast-[120%]"></Image>
             </Link>
         </div>
