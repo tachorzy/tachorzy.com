2429730a0d8e80ca634362d5ad8a9ac51efc2a9f
tachorzy <tachorzyad@gmail.com>
2024-11-16 15:52:30 -0600

    style: adjusted margin for travel section

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const sections: Map<string, Section> = new Map([
     ["home", {coreComponent: <LandingPageHeaders/>, trayComponent: <SocialsTray/>, nextPageReference: "#about", nextButtonMargins: "mt-[19rem] lg:mt-[15rem] xl:mt-[21.5rem] 2xl:mt-[19%]"}],
     ["about", { sectionTitle: "About Me", coreComponent: <AboutMe/>, pageNumber: "١", nextPageReference: "#projects", nextButtonMargins: "mt-2.5 xl:mt-[15%] 2xl:mt-[11.5%] 2xl:mb-36"}],
     ["projects", {sectionTitle: "Projects", sectionTitleMargin: "mb-7", coreComponent: <ProjectsTray/>, pageNumber: "٢", nextPageReference: "#travel", nextButtonMargins: "mt-2.5 lg:mt-72 lg:mb-16 xl:mt-[40%] 2xl:mt-[36%]"}],
-    ["travel", {margin: "mb-72 lg:mt-[40rem]", sectionTitle: "Travel", sectionTitleMargin: "mb-7", coreComponent: <CountryCardTray/>, pageNumber: "٣", nextButtonMargins: "xl:mt-48 2xl:mt-0" ,nextPageReference: "#contact"}],
+    ["travel", {margin: "mb-72 lg:mt-[20rem]", sectionTitle: "Travel", sectionTitleMargin: "mb-7", coreComponent: <CountryCardTray/>, pageNumber: "٣", nextButtonMargins: "xl:mt-48 2xl:mt-0" ,nextPageReference: "#contact"}],
     ["contact", {margin: "mb-72 ", sectionTitle: "Get in touch!", sectionTitleMargin: "mb-7", coreComponent: <ContactSection/>, pageNumber: "٤"}
   ]
   ]);
